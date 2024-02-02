c1791254506543fd786b2a420c4882781c630a04
Stolkerve <devsebasgr@gmail.com>
2024-02-02 09:44:58 -0400

    Apuntar axios hacia la ruta de api

--- client/src/axiosConfig.ts
+++ client/src/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:6969', // replace with your server's URL
+  baseURL: 'http://localhost:6969/api', // replace with your server's URL
 });
 
 export default instance;

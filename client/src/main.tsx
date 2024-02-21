c752fe4ce39731a3ec5b148bb384a51b1aa229a4
Sergionx <sergio180203@gmail.com>
2024-02-21 14:20:01 -0400

    Fix bug in minimap QueryClientProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// REVIEW - Bug en minimap QueryClientProvider si está estricto
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+  </>,
 )

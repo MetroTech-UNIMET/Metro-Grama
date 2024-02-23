108ce0e39432c26dac5ec2491e5f3a530c12ffd4
Sergionx <sergio180203@gmail.com>
2024-02-22 22:09:38 -0400

    Update background color and gradient in GraphLayout component

--- client/src/layouts/GraphLayout.tsx
+++ client/src/layouts/GraphLayout.tsx
@@ -6,7 +6,8 @@ export default function GraphLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-UI-black text-UI-white w-screen h-screen p-8">
+    <div className="bg-gradient-to-br from-primary-purple-900 to-[#1D1B32]
+      text-UI-white w-screen h-screen p-8">
       {children}
       <div className="fixed bottom-4 left-4">
         <h1 >LOGO METROTECH</h1>

60f63518912b9e82e9da096141c02085167331c7
Sergionx <sergio180203@gmail.com>
2024-03-15 13:42:45 -0400

    Refactor MenuActions component to improve code structure and add new features More control over the contextMenu

--- client/src/components/graph/behaviors/MenuActions.tsx
+++ client/src/components/graph/behaviors/MenuActions.tsx
@@ -1,56 +1,92 @@
-import { ContextMenuValue, GraphinContext } from "@antv/graphin";
-import { useContext, useEffect } from "react";
+import { ContextMenuValue, GraphinContext, IG6GraphEvent } from "@antv/graphin";
+import { useContext, useEffect, useRef, useState } from "react";
 import { ContextMenu } from "@antv/graphin-components";
 import { GraphinContextType } from "@antv/graphin";
 import { INode, IEdge } from "@antv/g6";
 
-import { ListContent, ListItem } from "../../ui/list";
+import { ListContent, ListHeader, ListItem } from "../../ui/list";
 import { markNodeAsViewed } from "@/lib/utils/states/NodeStates";
+import { cn } from "@/lib/utils/className";
 
-export default function MenuActions() {
+
+interface MenuNodeProps {
+  node: INode | null;
+  close: () => void;
+}
+
+function MenuNode({ node, close }: MenuNodeProps) {
+  if (!node) return null;
+
+  const subjectCode = node._cfg?.model?.data.Data.Code;
+  const subjectName = node._cfg?.model?.data.Data.Name;
+
   return (
-    <ContextMenu
-      bindType="node"
-      style={{
-        background: " ",
-        width: " ",
-      }}
-    >
-      <MenuNode />
-    </ContextMenu>
+    <ListContent>
+      <ListHeader>
+        {subjectCode} - {subjectName}
+      </ListHeader>
+      <ListItem
+        onClick={() => {
+          markNodeAsViewed(node);
+          close()
+        }}
+      >
+        {node?.hasState("viewed")
+          ? "Desmarcar como materia vista"
+          : "Marcar como materia vista"}
+      </ListItem>
+    </ListContent>
   );
 }
 
-function MenuNode() {
-  // @ts-ignore Error de typeo de la librería de Graphini
-  const {
-    contextmenu: { node },
-    graph,
-  }: GraphinContextType & {
-    contextmenu: { node: ContextMenuValue };
-  } = useContext(GraphinContext);
-  const hola = useContext(GraphinContext);
-  console.log(hola);
+// TODO - Mejor manejo de posición como si fuera un tooltip
+// TODO - Bloquear el mover el grafo cuando el menu está abierto
+export default function MenuActions() {
+  const { graph }: GraphinContextType = useContext(GraphinContext);
+  const [node, setNode] = useState<INode | null>(null);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  
+  useEffect(() => {
+    function handleOpenContextMenu(e: IG6GraphEvent) {
+      e.preventDefault();
+      const { item } = e;
+
+      //   graph.set("contextmenu", {
+      //   node: {
+      //     visible: true,
+      //     x: e.canvasX,
+      //     y: e.canvasY,
+      //     item,
+      //   },
+      // });
+
+      setPosition({ x: e.canvasX, y: e.canvasY });
+      setNode(item as INode);
+    }
+
+    graph.on("node:contextmenu", handleOpenContextMenu);
+    graph.on("canvas:click", close);
+
+    return () => {
+      graph.off("node:contextmenu", handleOpenContextMenu);
+      graph.off("canvas:click", close);
+    };
+  }, []);
+
+  function close() {
+    setNode(null);
+    setPosition({ x: 0, y: 0 });
+  }
 
   return (
-    // <DropdownMenuContent>
-    //   <DropdownMenuLabel>My Account</DropdownMenuLabel>
-    //   <DropdownMenuSeparator />
-    //   <DropdownMenuItem>Profile</DropdownMenuItem>
-    //   <DropdownMenuItem>Billing</DropdownMenuItem>
-    //   <DropdownMenuItem>Team</DropdownMenuItem>
-    //   <DropdownMenuItem>Subscription</DropdownMenuItem>
-    // </DropdownMenuContent>
-    <ListContent>
-      <ListItem
-        onClick={() => {
-          markNodeAsViewed(node.item as INode);
-        }}
-      >
-        Marcar como materia vista
-      </ListItem>
-    </ListContent>
+    <div
+      className={cn("absolute", !node && "hidden")}
+      style={{
+        top: position.y,
+        left: position.x,
+      }}
+    >
+      <MenuNode node={node} close={close} />
+    </div>
   );
 }

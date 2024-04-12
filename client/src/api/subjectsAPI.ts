cb12c7d5a7e6ae7281a43d0bd330c892bf9b6d5f
Sergionx <sergio180203@gmail.com>
2024-04-12 18:01:58 -0400

    Add Subject import to subjectsAPI.ts

--- client/src/api/subjectsAPI.ts
+++ client/src/api/subjectsAPI.ts
@@ -1,4 +1,5 @@
 import axios from "@/axiosConfig";
+import { Subject } from "@/interfaces/Subject";
 
 export async function getSubjects(): Promise<Graph<Subject>> {
   const response = await axios.get("/subjects/sistemas");

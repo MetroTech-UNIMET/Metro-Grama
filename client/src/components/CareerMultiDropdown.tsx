a4e09519893e77a1fdeaad47aeed42c39e452bb4
Sergionx <sergio180203@gmail.com>
2024-04-12 18:01:36 -0400

    Add Multidropdown responsible for fetching all the careers

---
+++ client/src/components/CareerMultiDropdown.tsx
@@ -0,0 +1,62 @@
+import { getCareers } from "@/api/careersApi";
+import { Career } from "@/interfaces/Career";
+import MultipleSelector, { Option } from "@ui/multidropdown";
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+interface Props {
+  value: Option[];
+  onChange: (value: Option[]) => void;
+  maxSelected?: number;
+}
+
+export function CareerMultiDropdown({
+  value,
+  onChange,
+  maxSelected = 2,
+}: Props) {
+  const { data, isLoading, error } = useQuery<Career[]>(
+    ["careers"],
+    getCareers
+  );
+
+  if (error) return null;
+
+  console.log(data);
+  const options =
+    data?.map((career) => ({
+      value: career.id,
+      label: career.name,
+    })) ?? [];
+
+  return (
+    <MultipleSelector
+      value={value}
+      onChange={onChange}
+      options={options}
+      maxSelected={maxSelected}
+      placeholder={
+        value.length === maxSelected
+          ? "Máximo alcanzado"
+          : "Selecciona las carreras que deseas visualizar"
+      }
+      // TODO Spinner
+      emptyIndicator={
+        isLoading ? (
+          "loading..."
+        ) : (
+          <p className="text-lg ">
+            {options.length === 0
+              ? " No se encontraron carreras. Por favor, intenta más tarde o recarga la página."
+              : "No hay más carreras para seleccionar."}
+          </p>
+        )
+      }
+      commandProps={{
+        className: "absolute z-10 max-w-sm h-auto pl",
+      }}
+      badgeClassName="bg-blue-300 text-black"
+      className="bg-gray-200"
+    />
+  );
+}
--- client/src/components/graph/Graph.tsx
+++ client/src/components/graph/Graph.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { getSubjects } from "@/api/subjectsAPI";
 import Graphin from "@antv/graphin";
 import { MiniMap } from "@antv/graphin-components";
@@ -6,15 +7,19 @@ import { useQuery } from "react-query";
 import SearchPrelations from "./behaviors/Search-Prelations";
 import MenuActions from "./behaviors/MenuActions";
 import useSubectGraph from "@/hooks/useSubectGraph";
-import { ShowAxiosError } from "@/components/ShowAxiosError";
+import { ShowAxiosError } from "@components/ShowAxiosError";
 import { AxiosError } from "axios";
 import { Subject } from "@/interfaces/Subject";
+import { CareerMultiDropdown } from "@components/CareerMultiDropdown";
+import { Option } from "@ui/multidropdown";
 
 export default function Graph() {
   const { data, isLoading, error } = useQuery<Graph<Subject>>(
     ["subjects", "Ingeniería en Sistemas"],
     getSubjects
   );
+
+  const [selectedCareers, setSelectedCareers] = useState<Option[]>([]);
 
   if (error) return <ShowAxiosError error={error as AxiosError} />;
 
@@ -23,25 +28,31 @@ export default function Graph() {
   const { graph } = useSubectGraph(data);
 
   return (
-    <Graphin
-      data={graph}
-      style={{
-        backgroundColor: "transparent",
-      }}
-      
-    >
-      <SearchPrelations />
-      <MenuActions />
-      <MiniMap
-        visible={true}
-        style={{
-          borderRadius: 4,
-        }}
-        options={{
-          className:
-            "fixed bottom-4 right-4 border-2 border-white rounded-lg bg-white",
-        }}
+    <>
+      <CareerMultiDropdown
+        value={selectedCareers}
+        onChange={setSelectedCareers}
       />
-    </Graphin>
+
+      <Graphin
+        data={graph}
+        style={{
+          backgroundColor: "transparent",
+        }}
+      >
+        <SearchPrelations />
+        <MenuActions />
+        <MiniMap
+          visible={true}
+          style={{
+            borderRadius: 4,
+          }}
+          options={{
+            className:
+              "fixed bottom-4 right-4 border-2 border-white rounded-lg bg-white",
+          }}
+        />
+      </Graphin>
+    </>
   );
 }

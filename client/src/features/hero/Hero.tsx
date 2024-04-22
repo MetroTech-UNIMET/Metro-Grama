13b0b28adfb1265f20643239e34d5abe900ae0f2
Sergionx <sergio180203@gmail.com>
2024-04-22 10:22:54 -0400

    Refactor Hero component to use CloudBlur component for background images

--- client/src/features/hero/Hero.tsx
+++ client/src/features/hero/Hero.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils/className";
 import { Link } from "react-router-dom";
 
 export default function Hero() {
@@ -9,18 +10,14 @@ export default function Hero() {
           alt=""
           className="absolute inset-0 -z-10 h-full w-full object-cover"
         />
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          />
-        </div>
+        <CloudBlur
+          containerClassName="-top-40 sm:-top-80"
+          blurClassName="left-[calc(50%+3rem)] sm:left-[calc(50%-30rem)]"
+        />
+        <CloudBlur
+          containerClassName="top-[calc(100%-13rem)] sm:top-[calc(100%-30rem)]"
+          blurClassName=" left-[calc(50%-11rem)] sm:left-[calc(50%+36rem)]"
+        />
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
           <div className="text-center">
@@ -48,19 +45,36 @@ export default function Hero() {
             </div>
           </div>
         </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          />
-        </div>
       </div>
+    </div>
+  );
+}
+
+function CloudBlur({
+  containerClassName = "",
+  blurClassName = "",
+}: {
+  containerClassName?: string;
+  blurClassName?: string;
+}) {
+  return (
+    <div
+      className={cn(
+        "absolute inset-x-0 -z-10 transform-gpu overflow-hidden blur-3xl",
+        containerClassName
+      )}
+      aria-hidden="true"
+    >
+      <div
+        className={cn(
+          "relative aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:w-[72.1875rem]",
+          blurClassName
+        )}
+        style={{
+          clipPath:
+            "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+        }}
+      />
     </div>
   );
 }

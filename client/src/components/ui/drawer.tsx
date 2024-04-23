4d07c7e28be7decf4e36cf7af98d461878feb3ec
Sergionx <sergio180203@gmail.com>
2024-04-22 22:08:31 -0400

    Add Context for custom styling depending of the orientation

--- client/src/components/ui/drawer.tsx
+++ client/src/components/ui/drawer.tsx
@@ -3,14 +3,35 @@ import { Drawer as DrawerPrimitive } from "vaul";
 
 import { cn } from "@utils/className";
 
+type Orientation = "horizontal" | "vertical";
+type Direction = "left" | "right" | "top" | "bottom";
+
+const DrawerContext = React.createContext<{
+  orientation: Orientation;
+  direction: Direction;
+}>({
+  orientation: "horizontal",
+  direction: "left",
+});
+
 const Drawer = ({
   shouldScaleBackground = true,
+  orientation = "horizontal",
   ...props
-}: React.ComponentProps<typeof DrawerPrimitive.Root>) => (
-  <DrawerPrimitive.Root
-    shouldScaleBackground={shouldScaleBackground}
-    {...props}
-  />
+}: React.ComponentProps<typeof DrawerPrimitive.Root> & {
+  orientation?: Orientation;
+}) => (
+  <DrawerContext.Provider
+    value={{
+      orientation,
+      direction: props.direction ?? "left",
+    }}
+  >
+    <DrawerPrimitive.Root
+      shouldScaleBackground={shouldScaleBackground}
+      {...props}
+    />
+  </DrawerContext.Provider>
 );
 Drawer.displayName = "Drawer";
 
@@ -35,22 +56,54 @@ DrawerOverlay.displayName = DrawerPrimitive.Overlay.displayName;
 const DrawerContent = React.forwardRef<
   React.ElementRef<typeof DrawerPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Content>
->(({ className, children, ...props }, ref) => (
-  <DrawerPortal>
-    <DrawerOverlay />
-    <DrawerPrimitive.Content
-      ref={ref}
-      className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
-        className
-      )}
-      {...props}
-    >
-      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted" />
-      {children}
-    </DrawerPrimitive.Content>
-  </DrawerPortal>
-));
+>(({ className, children, ...props }, ref) => {
+  const { orientation, direction } = React.useContext(DrawerContext);
+
+  return (
+    <DrawerPortal>
+      <DrawerOverlay />
+      <DrawerPrimitive.Content
+        ref={ref}
+        className={cn(
+          "fixed z-50 flex h-auto flex-col rounded-t-[10px] border bg-background",
+          orientation === "horizontal"
+            ? "inset-x-0 bottom-0 mt-24"
+            : "top-0 w-screen max-w-md h-full",
+
+          direction === "left" && orientation === "vertical" && "left-0",
+          direction === "right" && orientation === "vertical" && "right-0",
+
+          className
+        )}
+        {...props}
+      >
+        <div className="relative h-full">
+          <div
+            className={cn(
+              "mx-auto mt-4  rounded-full bg-muted",
+              orientation === "horizontal"
+                ? "w-[100px] h-2"
+                : "w-2 h-[100px] absolute top-1/2 -translate-y-1/2",
+
+              direction === "left" && orientation === "vertical" && "right-2",
+              direction === "right" && orientation === "vertical" && "left-2"
+            )}
+          />
+
+          <main
+            className={cn(
+              "h-full",
+              direction === "left" && orientation === "vertical" && "pr-6",
+              direction === "right" && orientation === "vertical" && "pl-6"
+            )}
+          >
+            {children}
+          </main>
+        </div>
+      </DrawerPrimitive.Content>
+    </DrawerPortal>
+  );
+});
 DrawerContent.displayName = "DrawerContent";
 
 const DrawerHeader = ({

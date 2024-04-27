9f88e40659784d3a339fc0d909615fa13fe7b2c9
Sergionx <sergio180203@gmail.com>
2024-04-26 23:15:10 -0400

    Moved graph to its own layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,23 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Grafo from "./features/grafo/Grafo";
-import GraphLayout from "./layouts/GraphLayout";
+import GraphLayout from "@/layouts/GraphLayout";
+import BasicLayout from "@/layouts/BasicLayout";
 
-import Navbar from "./features/hero/Navbar";
-import { Principal } from "./features/Principal/Principal";
-import Login from "./features/login-register/Login";
+import Grafo from "@/features/grafo/Grafo";
+import { Principal } from "@/features/Principal/Principal";
+import Login from "@/features/login-register/Login";
 
 function App() {
   const router = createBrowserRouter([
     {
-      element: <Navbar />,
+      element: <BasicLayout />,
       children: [
         { path: "/", element: <Principal /> },
         { path: "/login", element: <Login /> },
-
-        {
-          element: <GraphLayout />,
-
-          children: [{ path: "/materias", element: <Grafo /> }],
-        },
       ],
+    },
+    {
+      element: <GraphLayout />,
+      children: [{ path: "/materias", element: <Grafo /> }],
     },
   ]);
   return (

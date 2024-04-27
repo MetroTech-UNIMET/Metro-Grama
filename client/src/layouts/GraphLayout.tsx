2d81ada2454e0b9999b8186641a5b4443cf04d53
Sergionx <sergio180203@gmail.com>
2024-04-26 22:43:12 -0400

    Fix background color class name in GraphLayout component

--- client/src/layouts/GraphLayout.tsx
+++ client/src/layouts/GraphLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function GraphLayout() {
   return (
     <div
-      className="bg-gradient-to-b from-primary-purple-900 to-[#1D1B32]
+      className="bg-gradient-to-b from-primary-900 to-[#1D1B32]
       text-UI-white w-screen h-screen p-8"
     >
       <Outlet />

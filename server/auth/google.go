8e73f2ec178e06284fe8d5b13321a11888707e15
Sebastian Gonzalez <devsebasgr@gmail.com>
2024-05-20 15:46:43 -0400

    Move hardcode values to env values

--- server/auth/google.go
+++ server/auth/google.go
@@ -1,7 +1,8 @@
 package auth
 
 import (
-	"os"
+	"fmt"
+	"metrograma/env"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -11,9 +12,9 @@ var GoogleOauthConfig *oauth2.Config
 
 func InitGoogleOauth() {
 	GoogleOauthConfig = &oauth2.Config{
-		RedirectURL:  "http://localhost:6969/api/auth/google/callback",
-		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
-		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		RedirectURL:  fmt.Sprintf("%s/api/auth/google/callback", env.GetDotEnv("SERVER_ADDRS")),
+		ClientID:     env.GetDotEnv("GOOGLE_CLIENT_ID"),
+		ClientSecret: env.GetDotEnv("GOOGLE_CLIENT_SECRET"),
 		Scopes: []string{
 			"https://www.googleapis.com/auth/userinfo.email",
 			"https://www.googleapis.com/auth/userinfo.profile",

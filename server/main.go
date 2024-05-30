c842ffce0823c4b8ea9a40c7a4e331ca3eb8d56e
Sebastian Gonzalez <devsebasgr@gmail.com>
2024-05-29 22:25:05 -0400

    static serve

--- server/main.go
+++ server/main.go
@@ -28,12 +28,26 @@ func main() {
 	e.Use(echoMiddleware.Logger())
 	e.Use(echoMiddleware.Gzip())
 	e.Use(echoMiddleware.Decompress())
+	e.Use(echoMiddleware.StaticWithConfig(echoMiddleware.StaticConfig{
+		Skipper: nil,
+		// Root directory from where the static content is served.
+		Root: "www-build",
+		// Index file for serving a directory.
+		// Optional. Default value "index.html".
+		Index: "index.html",
+		// Enable HTML5 mode by forwarding all not-found requests to root so that
+		// SPA (single-page application) can handle the routing.
+		HTML5:      true,
+		Browse:     false,
+		IgnoreBase: false,
+		Filesystem: nil,
+	}))
 
 	handlers.CreateHandlers(e)
 
 	// Servir el frontend ya compilado en todas las rutas no tomadas
 	// Ya el frontend se encargara de manejarlas con react router
-	e.Static("/*", "www-build")
+	// e.Static("/*", "www-build")
 
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", env.GetDotEnv("PORT"))))
 

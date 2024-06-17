7c0033fe7d257b75c98a51b295fcaf9cf3f3bbfa
Sergionx <sergio180203@gmail.com>
2024-06-16 21:27:17 -0400

    first attempt into reworking status

--- client/src/components/graph/behaviors/StatusActions.tsx
+++ client/src/components/graph/behaviors/StatusActions.tsx
@@ -1,13 +1,23 @@
 import { createContext, useContext, useState } from "react";
 import { INode, IEdge } from "@antv/g6";
 import { getNodesFromEdges } from "@/lib/utils/graph";
+import { Subject } from "@/interfaces/Subject";
 
-type nodeCustomState = "accesible" | "viewed";
-type edgeCustomState = "future" | "prelation" | "prelation-viewed";
+export type nodeCustomState = "accesible" | "viewed";
+export type edgeCustomState = "future" | "prelation" | "prelation-viewed";
 
-interface StatusActionsContextProps {
-  nodeStatuses: NodeStatuses;
-  edgeStatuses: EdgeStatuses;
+export type newNodeState =
+  | {
+      state: nodeCustomState;
+      value: boolean;
+    }
+  | {
+      state: nodeCustomState;
+      value: boolean;
+    }[];
+
+interface StatusActionsContextProps<NodeStatusObject = Subject> {
+  nodeStatuses: NodeStatuses<NodeStatusObject>;
   nodeActions: {
     enableViewedNode: (
       node: INode,
@@ -19,6 +29,14 @@ interface StatusActionsContextProps {
     };
     disableViewedNode: (node: INode, outEdges: IEdge[]) => void;
   };
+  changeNodeState: (node: INode, newState: newNodeState) => void;
+  changeNodesState: (
+    nodesState: {
+      node: INode;
+      newState: newNodeState;
+    }[]
+  ) => void;
+
   edgeActions: {};
   graphActions: {};
 }
@@ -38,12 +56,12 @@ export function useStatusActions() {
   return statusActionsContext;
 }
 
-type NodeStatuses = {
-  [key in nodeCustomState]: string[];
+type NodeStatuses<T> = {
+  [key in nodeCustomState]: T[];
 };
 
-type EdgeStatuses = {
-  [key in edgeCustomState]: string[];
+type NodeStatusesExperiment<T> = {
+  [key in nodeCustomState]: Map<string, T>;
 };
 
 //@ts-ignore
@@ -59,37 +77,29 @@ interface ClearStateOptions {
 }
 
 export function StatusActions({ children }: { children: React.ReactNode }) {
-  const [nodeStatuses, setNodeStatuses] = useState<NodeStatuses>({
+  const [nodeStatuses, setNodeStatuses] = useState<NodeStatuses<Subject>>({
     accesible: [],
     viewed: [],
   });
 
-  const [edgeStatuses, setEdgeStatuses] = useState<EdgeStatuses>({
-    future: [],
-    prelation: [],
-    "prelation-viewed": [],
+  const [experimentStatus, setExperimentStatus] = useState<
+    NodeStatusesExperiment<Subject>
+  >({
+    accesible: new Map(),
+    viewed: new Map(),
   });
 
-  function changeNodeState(
-    node: INode,
-    newState:
-      | {
-          state: nodeCustomState;
-          value: boolean;
-        }
-      | {
-          state: nodeCustomState;
-          value: boolean;
-        }[]
-  ) {
+  function changeNodeState(node: INode, newState: newNodeState) {
+    const subject = (node.getModel().data as Node4j<Subject>).data;
+
     if (Array.isArray(newState)) {
-      const newNodeStatuses: Record<string, string[]> = {};
+      const newNodeStatuses: Record<string, Subject[]> = {};
 
       newState.forEach(({ state, value }) => {
         node.setState(state, value);
         newNodeStatuses[state] = value
-          ? [...nodeStatuses[state], node.getID()]
-          : nodeStatuses[state].filter((id) => id !== node.getID());
+          ? [...nodeStatuses[state], subject]
+          : nodeStatuses[state].filter((n) => n.code !== subject.code);
       });
 
       setNodeStatuses((prev) => ({
@@ -105,8 +115,8 @@ export function StatusActions({ children }: { children: React.ReactNode }) {
 
     const newNodeStatuses = {
       [state]: value
-        ? [...nodeStatuses[state], node.getID()]
-        : nodeStatuses[state].filter((id) => id !== node.getID()),
+        ? [...nodeStatuses[state], subject]
+        : nodeStatuses[state].filter((n) => n.code !== subject.code),
     };
 
     setNodeStatuses((prev) => ({
@@ -115,86 +125,150 @@ export function StatusActions({ children }: { children: React.ReactNode }) {
     }));
   }
 
-  //@ts-ignore
-  function changeEdgeState(
-    edge: IEdge,
-    newState:
-      | {
-          state: edgeCustomState;
-          value: boolean;
-        }
-      | {
-          state: edgeCustomState;
-          value: boolean;
-        }[]
+  function changeNodesState(
+    nodesState: {
+      node: INode;
+      newState: newNodeState;
+    }[]
   ) {
-    if (Array.isArray(newState)) {
-      const newEdgeStatuses: Record<string, string[]> = {};
+    const newNodeStatuses: Record<
+      nodeCustomState,
+      Map<string, Subject>
+    > = experimentStatus;
 
-      newState.forEach(({ state, value }) => {
-        edge.setState(state, value);
-        newEdgeStatuses[state] = value
-          ? [...edgeStatuses[state], edge.getID()]
-          : edgeStatuses[state].filter((id) => id !== edge.getID());
-      });
+    nodesState.forEach(({ node, newState }) => {
+      const subject = (node.getModel().data as Node4j<Subject>).data;
+      const nodeId = node.getID();
 
-      setEdgeStatuses((prev) => ({
-        ...prev,
-        ...newEdgeStatuses,
-      }));
+      if (Array.isArray(newState)) {
+        newState.forEach(({ state, value }) => {
+          node.setState(state, value);
 
-      return;
-    }
+          if (value) {
+            newNodeStatuses[state].set(nodeId, subject);
+          } else {
+            newNodeStatuses[state].delete(nodeId);
+          }
+        });
 
-    const { state, value } = newState;
-    edge.setState(state, value);
+        return;
+      }
 
-    const newEdgeStatuses = {
-      [state]: value
-        ? [...edgeStatuses[state], edge.getID()]
-        : edgeStatuses[state].filter((id) => id !== edge.getID()),
-    };
+      const { state, value } = newState;
+      node.setState(state, value);
 
-    setEdgeStatuses((prev) => ({
-      ...prev,
-      ...newEdgeStatuses,
-    }));
+      if (value) {
+        newNodeStatuses[state].set(nodeId, subject);
+      } else {
+        newNodeStatuses[state].delete(nodeId);
+      }
+    });
+
+    setExperimentStatus(newNodeStatuses);
   }
 
-  function enableViewedNode(
-    node: INode,
-    firstNode = true,
-    viewedNodes: Set<string> = new Set()
-  ) {
-    const outEdges = node.getOutEdges();
-
+  function enableViewedNode(node: INode) {
     if (node.hasState("viewed")) {
-      if (firstNode)
-        return { nodes: disableViewedNode(node, outEdges), enabled: false };
+      return { nodes: disableViewedNode(node, outEdges), enabled: false };
     }
 
-    const nodeId = node.getID();
-    if (viewedNodes.has(nodeId)) {
-      return { nodes: viewedNodes, enabled: true };
+    const nodesState: {
+      node: INode;
+      newState: newNodeState;
+    }[] = [
+      {
+        node,
+        newState: [
+          { state: "accesible", value: false },
+          { state: "viewed", value: true },
+        ],
+      },
+    ];
+
+    // REVIEW - Nodos repetidos
+    nodesState.push(...enableChildrenNodes(node));
+
+    changeNodesState(nodesState);
+    console.log("Debug", nodesState);
+
+    return {
+      nodes: new Set(nodesState.map((n) => n.node.getID())),
+      enabled: true,
+    };
+  }
+
+  function enableChildrenNodes(
+    currentNode: INode,
+    nodesState: {
+      node: INode;
+      newState: newNodeState;
+    }[] = []
+  ) {
+    const inEdges = currentNode.getInEdges();
+    
+    const previousNodes = getNodesFromEdges(inEdges, "source");
+    previousNodes.forEach((node) => {
+      if (node.hasState("viewed")) return;
+      
+      const outEdges = node.getOutEdges();
+      nodesState.push({
+        node,
+        newState: [
+          { state: "accesible", value: false },
+          { state: "viewed", value: true },
+        ],
+      });
+
+      if (node.getID() === "subject:FBTMM01") {
+        // debugger;
+      }
+
+      enableChildrenNodes(node, nodesState);
+      nodesState.push(...checkAccesible2(outEdges, nodesState));
+    });
+
+    return nodesState;
+  }
+
+  function disableViewedNode2(
+    node: INode,
+    outEdges: IEdge[],
+    nodesState: {
+      node: INode;
+      newState: newNodeState;
+    }[]
+  ) {
+    const sourceNodes = getNodesFromEdges(node.getInEdges(), "source");
+
+    const shouldAccesible = sourceNodes.every((node) =>
+      node.hasState("viewed")
+    );
+
+    const isViewed = node.hasState("viewed");
+    const isAccesible = node.hasState("accesible");
+
+    if (isAccesible && !isViewed) {
+      changeNodeState(node, [
+        { state: "accesible", value: false },
+        { state: "viewed", value: false },
+      ]);
+      return disabledNodes;
+    } else if (!node.hasState("viewed")) {
+      return disabledNodes;
     }
 
     changeNodeState(node, [
-      { state: "accesible", value: false },
-      { state: "viewed", value: true },
+      { state: "accesible", value: shouldAccesible },
+      { state: "viewed", value: false },
     ]);
+    disabledNodes.add(node.getID());
 
-    viewedNodes.add(nodeId);
+    const outNodes = getNodesFromEdges(outEdges, "target");
 
-    const inEdges = node.getInEdges();
-
-    const previousNodes = getNodesFromEdges(inEdges, "source");
-    previousNodes.forEach(
-      (node) =>
-        !node.hasState("viewed") && enableViewedNode(node, false, viewedNodes)
+    outNodes.forEach((node) =>
+      disableViewedNode(node, node.getOutEdges(), disabledNodes)
     );
-
-    checkAccesible(outEdges);
-    return { nodes: viewedNodes, enabled: true };
+    return disabledNodes;
   }
 
   // TODO - Esto rrecorre todo el grafo hacia abajo, parar si el nodo xxx
@@ -220,6 +294,7 @@ export function StatusActions({ children }: { children: React.ReactNode }) {
     } else if (!node.hasState("viewed")) {
       return disabledNodes;
     }
+
     changeNodeState(node, [
       { state: "accesible", value: shouldAccesible },
       { state: "viewed", value: false },
@@ -234,43 +309,56 @@ export function StatusActions({ children }: { children: React.ReactNode }) {
     return disabledNodes;
   }
 
-  function checkAccesible(outEdges: IEdge[]) {
+  function checkAccesible2(
+    outEdges: IEdge[],
+    nodesState: {
+      node: INode;
+      newState: newNodeState;
+    }[]
+  ) {
     const nodesToCheck = getNodesFromEdges(outEdges, "target");
 
     nodesToCheck.forEach((node) => {
       const sourceNodes = getNodesFromEdges(node.getInEdges(), "source");
 
-      if (
-        sourceNodes.every((node) => node.hasState("viewed")) &&
-        !node.hasState("viewed")
-      ) {
-        changeNodeState(node, { state: "accesible", value: true });
+      // const sourceNodesViewed = sourceNodes.every((node) =>
+      //   node.hasState("viewed")
+      // );
+
+      // FIXME - Race condition, en vez de pedir nodesState, pedir un objeto parecido a experimentStatus en los parametros.
+      const sourceNodesViewed = sourceNodes.every((node) =>
+        // experimentStatus.viewed.has(node.getID())
+        nodesState.some(
+          (n) =>
+            n.node.getID() === node.getID() &&
+            n.newState.some((s) => s.state === "viewed" && s.value)
+        )
+      );
+      const isViewed = node.hasState("viewed");
+
+      if (sourceNodesViewed && !isViewed) {
+        nodesState.push({
+          node,
+          newState: [{ state: "accesible", value: true }],
+        });
+        checkAccesible2(node.getOutEdges(), nodesState);
       }
     });
+
+    return nodesState;
   }
-
-  // function clearGraphStates(graph: IGraph, options: ClearStateOptions = {}) {
-  //   clearEdgesStates(graph, options);
-  //   clearNodesStates(graph, options);
-  // }
-
-  // function clearEdgesStates(graph: IGraph, options: ClearStateOptions) {
-  //   throw new Error("Function not implemented.");
-  // }
-
-  // function clearNodesStates(graph: IGraph, options: ClearStateOptions) {
-  //   throw new Error("Function not implemented.");
-  // }
 
   return (
     <StatusActionsContext.Provider
       value={{
         nodeStatuses,
-        edgeStatuses,
         nodeActions: {
           enableViewedNode,
           disableViewedNode,
         },
+        changeNodeState,
+        changeNodesState,
+
         edgeActions: {},
         graphActions: {},
       }}

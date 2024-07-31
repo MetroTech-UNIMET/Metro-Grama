81c8c3b73f47bbdcf9e1d316d726cd38fa5d6e09
Sergionx <sergio180203@gmail.com>
2024-07-30 20:35:43 -0400

    feat: Hook to count the credits and syncrhonyze the subjects that depend on it

---
+++ client/src/features/grafo/hooks/useCountCredits.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useMemo } from "react";
+import { useStatusActions } from "../../../components/graph/behaviors/StatusActions";
+import { Subject } from "@/interfaces/Subject";
+import type { IGraph, INode } from "@antv/g6";
+
+export default function useCountCredits(graph: IGraph) {
+  const { nodeStatuses, subjectWithCredits, changeNodeState } =
+    useStatusActions();
+
+  const [credits, BPCredits] = useMemo(() => {
+    let credits = 0;
+    let BPCredits = 0;
+
+    nodeStatuses.viewed.forEach((subject) => {
+      credits += 3;
+
+      if (getSubjectType(subject) === "BP") {
+        BPCredits += 4;
+      }
+    });
+
+    return [credits, BPCredits];
+  }, [nodeStatuses.viewed]);
+
+  // TODO - Hacer un toast que desbloquearon materias por créditos si mínimo una materia se desbloquea
+  useEffect(() => {
+    subjectWithCredits.forEach((subject) => {
+      const needCredits = subject.credits !== 0;
+      const needBPCredits = subject.BPCredits !== 0;
+
+      const hasEnoughBP = BPCredits >= subject.BPCredits;
+      const hasEnoughCredits = credits >= subject.credits;
+
+      if (
+        (!needCredits && hasEnoughBP) ||
+        (!needBPCredits && hasEnoughCredits) ||
+        (needCredits && needBPCredits && (hasEnoughBP || hasEnoughCredits))
+      ) {
+        const node = graph.findById(`subject:${subject.code}`) as INode;
+        changeNodeState({
+          node,
+          newState: { state: "accesible", value: true },
+        });
+
+        return;
+      }
+
+      const hasLessCredits = subject.credits > 0 && credits < subject.credits;
+      const hasLessBPCredits =
+        subject.BPCredits > 0 && BPCredits < subject.BPCredits;
+
+      const nodeId = `subject:${subject.code}`;
+      if (
+        nodeStatuses.accesible.has(nodeId) &&
+        (hasLessCredits || hasLessBPCredits)
+      ) {
+        const node = graph.findById(nodeId) as INode;
+
+        changeNodeState({
+          node,
+          newState: { state: "accesible", value: false },
+        });
+      }
+    });
+  }, [credits, subjectWithCredits]);
+
+  return {
+    credits,
+    BPCredits,
+  };
+}
+
+function getSubjectType(subject: Subject) {
+  return subject.code.slice(0, 2);
+}

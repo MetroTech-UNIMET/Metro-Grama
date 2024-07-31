2703e98c212e027f03e48f5af722eabbf157a2e2
Sergionx <sergio180203@gmail.com>
2024-07-30 20:22:21 -0400

    refactor: Update CreditsMenu component to count and display credits
    The CreditsMenu component in CreditsMenu.tsx has been updated to count and display the number of credits and BPCredits. This change enhances the functionality of the component by providing accurate credit information to the user.

--- client/src/components/graph/behaviors/CreditsMenu.tsx
+++ client/src/components/graph/behaviors/CreditsMenu.tsx
@@ -1,10 +1,21 @@
+import { useState,useContext } from "react";
 import { cn } from "@utils/className";
 import { GripVertical } from "lucide-react";
-import { useState } from "react";
+import useCountCredits from "../../../features/grafo/hooks/useCountCredits";
+import { GraphinContext } from "@antv/graphin";
+
 
 export default function CreditsMenu() {
   const [open, setOpen] = useState(false);
 
+  const { graph } = useContext(GraphinContext);
+
+  const {BPCredits,credits}  = useCountCredits(graph);
+
+  // console.log({
+  //   BPCredits,
+  //   credits
+  // })
   return (
     <aside
       className={cn(
@@ -15,30 +26,8 @@ export default function CreditsMenu() {
       )}
     >
       <div className="bg-UI-white h-full text-black rounded-md w-80 transition-all duration-1000 ">
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
-        holaaaaaaaaaaaa
+        {credits}{" "}
+        {BPCredits}
         <button
           className="bg-UI-white p-2 rounded-l-md  h-10 
               fixed top-1/2 -translate-y-1/2 left-0 -translate-x-10"

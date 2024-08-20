2a92f4badeb7563d1e1aa50f9d91af792976788f
Sergionx <sergio180203@gmail.com>
2024-08-20 19:52:23 -0400

    feat: Add getSubjectsGraph function for fetching subject graph
    This commit adds a new function, getSubjectsGraph, to the subjectsAPI.ts file. The function fetches the subject graph data by making a GET request to the "/subjects/graph/" endpoint with the provided career parameter. This feature enhances the functionality of the code by allowing the retrieval of subject graph data for a specific career.

--- client/src/api/subjectsAPI.ts
+++ client/src/api/subjectsAPI.ts
@@ -1,10 +1,23 @@
 import axios from "@/axiosConfig";
 import type { Subject } from "@/interfaces/Subject";
 
-export async function getSubjects(
+export async function getSubjectsGraph(
   careerParam: string
 ): Promise<Graph<Subject>> {
   if (careerParam === "none") return { nodes: [], edges: [] };
+
+  const response = await axios.get("/subjects/graph/", {
+    params: {
+      careers: careerParam,
+    },
+  });
+
+  return response.data;
+}
+
+export async function getSubjects(
+  careerParam: string
+): Promise<Subject[]> {
 
   const response = await axios.get("/subjects/", {
     params: {

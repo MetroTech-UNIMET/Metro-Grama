32a102722d74f09336076552c25e229aba21f139
Sergionx <sergio180203@gmail.com>
2024-08-20 21:11:39 -0400

    refactor: Update FloatingLabelInput component to use FormField and FormControl
    This commit refactors the FloatingLabelInput component in the floating-label-input.tsx file. The changes include replacing the usage of the deprecated Input and Label components with the new FormField and FormControl components from the @ui/form module. This refactor enhances the maintainability and compatibility of the code by using the latest form-related components and APIs.

--- client/src/components/ui/derived/floating-label-input.tsx
+++ client/src/components/ui/derived/floating-label-input.tsx
@@ -2,55 +2,139 @@ import * as React from "react";
 
 import { cn } from "@utils/className";
 import { Input } from "@ui/input";
-import { Label } from "@ui/label";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@ui/form";
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+import type { FieldValues, Path } from "react-hook-form";
 
-const FloatingInput = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <Input
-        placeholder=" "
-        className={cn("peer", className)}
-        ref={ref}
-        {...props}
-      />
-    );
-  }
-);
-FloatingInput.displayName = "FloatingInput";
+type FloatingLabelInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  label?: string;
+  containerClassname?: string;
+  labelClassName?: string;
+};
 
-const FloatingLabel = React.forwardRef<
-  React.ElementRef<typeof Label>,
-  React.ComponentPropsWithoutRef<typeof Label>
->(({ className, ...props }, ref) => {
+interface FloatingLabelInputFieldProps<T extends FieldValues>
+  extends FloatingLabelInputProps {
+  name: Path<T>;
+  showErrors?: boolean;
+}
+
+function FloatingLabelInputField<T extends FieldValues>({
+  name,
+  disabled,
+  id,
+  label,
+  containerClassname,
+  labelClassName,
+  className,
+  showErrors = false,
+  ...props
+}: FloatingLabelInputFieldProps<T>) {
   return (
-    <Label
-      className={cn(
-        "peer-focus:secondary peer-focus:dark:secondary absolute start-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-background px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 dark:bg-background rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4",
-        className
-      )}
-      ref={ref}
-      {...props}
+    <FormField
+      name={name}
+      disabled={disabled}
+      render={({ field: { ref, onChange, ...field }, fieldState }) => {
+        const hasError = fieldState.invalid;
+        const isSuccessful = !fieldState.invalid && fieldState.isTouched;
+
+        return (
+          // <FormItem className={cn("relative", containerClassname)}>
+          //   <FormControl>
+          //     <FloatingInput
+          //       className={cn(
+          //         "read-only:cursor-not-allowed read-only:opacity-50",
+          //         {
+          //           "bg-destructive/10": hasError,
+          //           "bg-success/10": isSuccessful,
+          //         }
+          //       )}
+          //       onChange={(e) => {
+          //         if (props.type === "number") {
+          //           onChange(Number(e.target.value));
+          //         } else {
+          //           onChange(e);
+          //         }
+          //       }}
+          //       {...props}
+          //       {...field}
+          //     />
+          //   </FormControl>
+
+          //   <FormLabel
+          //     className={cn(
+          //       "peer-focus:secondary peer-focus:dark:secondary absolute start-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-background px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 dark:bg-background rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4",
+          //       {
+          //         "text-destructive/80": hasError,
+          //         "text-success/80": isSuccessful,
+          //       }
+          //     )}
+          //   >
+          //     {label}
+          //   </FormLabel>
+
+          //   {showErrors && hasError && (
+          //     <FormMessage className="text-destructive/80 text-sm mt-1">
+          //       {fieldState.error?.message}
+          //     </FormMessage>
+          //   )}
+          // </FormItem>
+
+          <FormItem className={cn("relative", containerClassname)}>
+            <FormControl>
+              <Input
+                className={cn(
+                  "block h-auto pb-2.5 pt-5 w-full text-gray-900 border-gray-300 appearance-none outline-none focus:ring-0 focus:border-primary-500 peer",
+                  {
+                    "bg-destructive/10 border-destructive": hasError,
+                    "bg-success/10 border-success": isSuccessful,
+                  },
+                  className
+                )}
+                onChange={(e) => {
+                  if (props.type === "number") {
+                    onChange(Number(e.target.value));
+                  } else {
+                    onChange(e);
+                  }
+                }}
+                placeholder={props.placeholder ?? " "}
+                {...props}
+                {...field}
+              />
+            </FormControl>
+
+            <FormLabel
+              className={cn(
+                "absolute text-sm duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-primary-500 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto",
+                {
+                  "-translate-y-4 scale-75": props.placeholder,
+                  "peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4":
+                    !props.placeholder,
+                },
+                labelClassName
+              )}
+            >
+              {label}
+            </FormLabel>
+
+            {showErrors && hasError && (
+              <FormMessage className="text-destructive/80 text-sm mt-1">
+                {fieldState.error?.message}
+              </FormMessage>
+            )}
+          </FormItem>
+        );
+      }}
     />
   );
-});
-FloatingLabel.displayName = "FloatingLabel";
+}
 
-type FloatingLabelInputProps = InputProps & { label?: string };
+FloatingLabelInputField.displayName = "FloatingLabelInputField";
 
-const FloatingLabelInput = React.forwardRef<
-  React.ElementRef<typeof FloatingInput>,
-  React.PropsWithoutRef<FloatingLabelInputProps>
->(({ id, label, ...props }, ref) => {
-  return (
-    <div className="relative">
-      <FloatingInput ref={ref} id={id} {...props} />
-      <FloatingLabel htmlFor={id}>{label}</FloatingLabel>
-    </div>
-  );
-});
-FloatingLabelInput.displayName = "FloatingLabelInput";
-
-export { FloatingInput, FloatingLabel, FloatingLabelInput };
+export { FloatingLabelInputField };

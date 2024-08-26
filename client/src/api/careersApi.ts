a212adcab08c28c8a8a06d19a800f17d4b025a04
Sergionx <sergio180203@gmail.com>
2024-08-26 14:19:36 -0400

    feat: Add endpoint to get complete career details
    The commit adds a new endpoint to the careers API that allows retrieving a career along with its associated subjects by ID. The endpoint is accessible at "/careers/withSubjects/:careerId" and returns the career details along with an array of subjects. This feature enhances the functionality of the careers API by providing a way to fetch a career and its subjects in a single request.

--- client/src/api/careersApi.ts
+++ client/src/api/careersApi.ts
@@ -1,5 +1,7 @@
 import axios from "@/axiosConfig";
-import type { Career } from "@/interfaces/Career";
+import { idToSurrealId } from "@utils/queries";
+
+import type { Career, CareerWithSubjects } from "@/interfaces/Career";
 
 export async function getCareers(): Promise<Career[]> {
   const response = await axios.get("/careers/");
@@ -7,7 +9,20 @@ export async function getCareers(): Promise<Career[]> {
   return response.data;
 }
 
-export async function createCareers(data: any) {
-  console.log(data)
+export async function createCareer(data: any) {
   return await axios.post("/careers/", data);
+}
+
+// TODO - Hacer endpoint
+export async function updateCareer(data: any) {
+  return await axios.put("/careers/", data);
+}
+
+export async function getCompleteCareer(id?: string): Promise<CareerWithSubjects> {
+  if (!id) throw new Error("No id provided");
+  
+  const surrealId = idToSurrealId(id, "career");
+  const response = await axios.get(`/careers/withSubjects/${surrealId}`);
+
+  return response.data;
 }
--- client/src/interfaces/Career.ts
+++ client/src/interfaces/Career.ts
@@ -1,5 +1,15 @@
+import type { Subject } from "./Subject";
+
 export interface Career {
   id: string;
   name: string;
   emoji: string;
+}
+
+type SubjectWithPrelations = Omit<Subject, 'careers'> & {
+  prelations: string[];
+}
+
+export interface CareerWithSubjects extends Career {
+  subjects: (SubjectWithPrelations | null)[][];
 }

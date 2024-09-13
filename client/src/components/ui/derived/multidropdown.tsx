9bb3245dbebd7a3325379d88ba3c93e0ca145d5a
Sergionx <sergio180203@gmail.com>
2024-09-13 17:04:28 -0400

    refactor: Update value prop in MultipleSelector component

--- client/src/components/ui/derived/multidropdown.tsx
+++ client/src/components/ui/derived/multidropdown.tsx
@@ -465,7 +465,7 @@ const MultipleSelector = React.forwardRef<
                           return (
                             <CommandItem
                               key={option.value}
-                              value={option.value}
+                              value={option.label}
                               disabled={option.disable}
                               onMouseDown={(e) => {
                                 e.preventDefault();

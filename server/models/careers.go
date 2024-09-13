5e219416fefff317e27f780c27bd513aa3882f3c
Sergionx <sergio180203@gmail.com>
2024-09-13 16:59:34 -0400

    refactor: Update Career and CareerNode structs in careers.go
    - Update Career struct to remove the ElectivesTrimesters field and embed CareerNode struct
    - Update CareerNode struct to add validation tags for ID, Name, and Emoji fields
    - Rename CareerForm to CareerCreateForm
    - Rename CareerSubject to CreateCareerSubject
    - Add CareerUpdateForm and UpdateCareerSubject structs for career update functionality
    - Update CareerSubjectWithoutType struct to remove the required validation tag for Prelations field
    - Update CareerWithSubjects struct to remove the ElectivesTrimesters field

--- server/models/careers.go
+++ server/models/careers.go
@@ -1,26 +1,24 @@
 package models
 
 type Career struct {
-	ElectivesTrimesters []int  `json:"electivesTrimesters"`
-	Emoji               string `json:"emoji"`
-	ID                  string `json:"id"`
-	Name                string `json:"name"`
+	ElectivesTrimesters []int `json:"electivesTrimesters"`
+	CareerNode
 }
 
 type CareerNode struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Emoji string `json:"emoji"`
+	ID    string `json:"id" validate:"required"`
+	Name  string `json:"name" validate:"required"`
+	Emoji string `json:"emoji" validate:"required"`
 }
 
-type CareerForm struct {
-	ID_Name  string             `validate:"required"`
-	Name     string             `json:"name" validate:"required"`
-	Emoji    string             `json:"emoji" validate:"required"`
-	Subjects [][]*CareerSubject `json:"subjects" validate:"required,dive,required"`
+type CareerCreateForm struct {
+	ID_Name  string                   `validate:"required"`
+	Name     string                   `json:"name" validate:"required"`
+	Emoji    string                   `json:"emoji" validate:"required"`
+	Subjects [][]*CreateCareerSubject `json:"subjects" validate:"required,dive,required"`
 }
 
-type CareerSubject struct {
+type CreateCareerSubject struct {
 	Code        string   `json:"code" validate:"required"`
 	Name        string   `json:"name" validate:"required"`
 	Credits     uint8    `json:"credits" validate:"gte=0,lte=150"`
@@ -29,12 +27,28 @@ type CareerSubject struct {
 	SubjectType string   `json:"subjectType" validate:"required,oneof=elective existing new"`
 }
 
+type CareerUpdateForm struct {
+	Name     string                               `json:"name"`
+	Emoji    string                               `json:"emoji"`
+	Subjects map[int]map[int]*UpdateCareerSubject `json:"subjects"`
+}
+
+type UpdateCareerSubject struct {
+	Code      string `json:"code" validate:"required"`
+	Name      string `json:"name"`
+	Credits   uint8  `json:"credits" validate:"gte=0,lte=150"`
+	BPCredits uint8  `json:"BPCredits" validate:"gte=0,lte=150"`
+	// Prelations  []string `json:"prelations" validate:"dive,required"`
+	Prelations  []string `json:"prelations"`
+	SubjectType string   `json:"subjectType" validate:"oneof=elective existing new"`
+}
+
 type CareerSubjectWithoutType struct {
 	Code       string   `json:"code" validate:"required"`
 	Name       string   `json:"name" validate:"required"`
 	Credits    uint8    `json:"credits" validate:"gte=0,lte=150"`
 	BPCredits  uint8    `json:"BPCredits" validate:"gte=0,lte=150"`
-	Prelations []string `json:"prelations" validate:"required,dive,required"`
+	Prelations []string `json:"prelations" validate:"required"`
 }
 
 type CareerWithSubjects struct {

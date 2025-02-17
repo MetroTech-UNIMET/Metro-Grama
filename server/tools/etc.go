0bdd9dd5edc0aa4a404f18ebfb3ec9209aaa58e0
Sergionx <sergio180203@gmail.com>
2025-02-17 16:58:33 -0400

    refactor: Remove surreal id handling function to use the surrealdb 2.0 functions

--- server/tools/etc.go
+++ server/tools/etc.go
@@ -1,22 +1,5 @@
 package tools
 
-import (
-	"fmt"
-	"strings"
-)
-
 func CreateMsg(msg string) map[string]string {
 	return map[string]string{"message": msg}
-}
-
-func ToID(table string, id string) string {
-	return fmt.Sprintf("%s:%s", table, id)
-}
-
-func FromID(id string) string {
-	parts := strings.Split(id, ":")
-	if len(parts) >= 1 {
-		return parts[1]
-	}
-	return ""
 }

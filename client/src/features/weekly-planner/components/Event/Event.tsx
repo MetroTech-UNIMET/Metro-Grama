4da0d98c307e36d9b41723e297511a399535d455
Sergionx <sergio180203@gmail.com>
2025-07-29 20:34:21 -0400

    feat: create BaseEvent component for event rendering and refactor PlannerEvent and MobileEvent to use it

--- client/src/features/weekly-planner/components/Event/Event.tsx
+++ client/src/features/weekly-planner/components/Event/Event.tsx
@@ -1,39 +1,26 @@
-import {
-  calculateCustomHeight,
-  calculateCustomTop,
-  calculateUniformHeight,
-  calculateUniformTop,
-} from "./functions";
-import { useWeeklyPlannerContext } from "../../context";
+import { calculateCustomHeight, calculateCustomTop, calculateUniformHeight, calculateUniformTop } from './functions';
+import { useWeeklyPlannerContext } from '../../context';
 
-import { cn } from "@utils/className";
+import { cn } from '@utils/className';
 
-import type { Event } from "../../types";
+import type { Event } from '../../types';
+import { BaseEvent } from './BaseEvent';
 
 interface Props {
   event: Event;
 }
 
 export function PlannerEvent({ event }: Props) {
-  const { start_hour: planner_start_hour, ...props } =
-    useWeeklyPlannerContext();
+  const { start_hour: planner_start_hour, ...props } = useWeeklyPlannerContext();
 
   const height =
-    props.type === "uniform-interval"
+    props.type === 'uniform-interval'
       ? calculateUniformHeight(event.start_hour, event.end_hour, props.interval)
-      : calculateCustomHeight(
-          event.start_hour,
-          event.end_hour,
-          props.timeSlots
-        );
+      : calculateCustomHeight(event.start_hour, event.end_hour, props.timeSlots);
 
   const top =
-    props.type === "uniform-interval"
-      ? calculateUniformTop(
-          event.start_hour,
-          planner_start_hour,
-          props.interval
-        )
+    props.type === 'uniform-interval'
+      ? calculateUniformTop(event.start_hour, planner_start_hour, props.interval)
       : calculateCustomTop(event.start_hour, props.timeSlots);
 
   // console.log(event.start_hour, event.end_hour, top);
@@ -45,39 +32,16 @@ export function PlannerEvent({ event }: Props) {
         className="md:px-px"
         style={
           {
-            "--w-schedule-event-top": `calc(3rem * ${top})`,
-            "--w-schedule-event-height": `calc(3rem * ${height})`,
+            '--w-schedule-event-top': `calc(3rem * ${top})`,
+            '--w-schedule-event-height': `calc(3rem * ${height})`,
           } as React.CSSProperties
         }
       >
-        <div
-          className={cn(
-            "w-full block p-4 rounded-md shadow-sm hover:shadow-md transition-all border-l-4",
-            "absolute h-[var(--w-schedule-event-height)] top-[var(--w-schedule-event-top)]",
-            getEventColor(event.type)
-          )}
-        >
-          <time className="text-sm opacity-70 block md:text-xs">
-            {event.start_hour} - {event.end_hour}
-          </time>
-          <div className="text-base md:text-lg font-medium">{event.title}</div>
-        </div>
+        <BaseEvent
+          event={event}
+          containerClassName="absolute top-[var(--w-schedule-event-top)] h-[var(--w-schedule-event-height)]"
+        />
       </li>
     </>
   );
 }
-
-const getEventColor = (type: Event["type"]) => {
-  switch (type) {
-    case "abs":
-      return "bg-amber-200  border-amber-400";
-    case "rowing":
-      return "bg-teal-800 text-white  border-teal-900";
-    case "yoga1":
-      return "bg-pink-200  border-pink-300";
-    case "restorative":
-      return "bg-green-200  border-green-300";
-    default:
-      return "bg-gray-200  border-gray-400";
-  }
-};
--- client/src/features/weekly-planner/components/Event/MobileEvent.tsx
+++ client/src/features/weekly-planner/components/Event/MobileEvent.tsx
@@ -1,38 +1,13 @@
-import { cn } from "@utils/className";
-import type { Event } from "../../types";
+import { BaseEvent } from './BaseEvent';
+import type { Event } from '../../types';
 
 interface Props {
   event: Event;
 }
 export function MobileEvent({ event }: Props) {
   return (
-    <li key={event.id}>
-      <div
-        className={cn(
-          "w-full block p-4 rounded-md shadow-sm hover:shadow-md transition-all border-l-4",
-          getEventColor(event.type)
-        )}
-      >
-        <time className="text-sm opacity-70 block md:text-xs">
-          {event.start_hour} - {event.end_hour}
-        </time>
-        <div className="text-base md:text-lg font-medium">{event.title}</div>
-      </div>
+    <li>
+      <BaseEvent event={event} />
     </li>
   );
 }
-
-const getEventColor = (type: Event["type"]) => {
-  switch (type) {
-    case "abs":
-      return "bg-amber-200  border-amber-400";
-    case "rowing":
-      return "bg-teal-800 text-white  border-teal-900";
-    case "yoga1":
-      return "bg-pink-200  border-pink-300";
-    case "restorative":
-      return "bg-green-200  border-green-300";
-    default:
-      return "bg-gray-200  border-gray-400";
-  }
-};

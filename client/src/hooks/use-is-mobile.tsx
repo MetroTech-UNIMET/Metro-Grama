a363ce9c4e5b7e18b890e51925508f1afc0374e0
Sergionx <sergio180203@gmail.com>
2025-07-29 22:43:13 -0400

    refactor: update useBreakpoint to accept dynamic breakpoint and improve mobile detection logic

--- client/src/hooks/use-is-mobile.tsx
+++ client/src/hooks/use-is-mobile.tsx
@@ -1,19 +1,23 @@
-import * as React from "react"
+import * as React from 'react';
 
-const MOBILE_BREAKPOINT = 768
+const MOBILE_BREAKPOINT = 768;
 
-export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+export function useBreakpoint(breakpoin: number) {
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const mql = window.matchMedia(`(max-width: ${breakpoin - 1}px)`);
     const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
+      setIsMobile(window.innerWidth < breakpoin);
+    };
+    mql.addEventListener('change', onChange);
+    setIsMobile(window.innerWidth < breakpoin);
+    return () => mql.removeEventListener('change', onChange);
+  }, [breakpoin]);
 
-  return !!isMobile
+  return !!isMobile;
+}
+
+export function useIsMobile() {
+  return useBreakpoint(MOBILE_BREAKPOINT);
 }

40f14323c8ca0d013e21f1580d993b1f3d357211
Sergionx <sergio180203@gmail.com>
2025-08-08 13:30:15 -0400

    feat: enhance AutoComplete component with grouping options and custom command item types

---
+++ client/src/components/ui/derived/custom-command-items/types.ts
@@ -0,0 +1,29 @@
+import type { Option } from '@/components/ui/types';
+import type { ComponentProps } from 'react';
+import { Command as CommandPrimitive } from 'cmdk';
+
+type ItemProps = ComponentProps<typeof CommandPrimitive.Item>;
+
+interface ForwardedProps {
+  id?: string;
+  'cmdk-item'?: string;
+  role?: 'option';
+
+  'aria-disabled'?: boolean;
+  'aria-selected'?: boolean;
+  'data-disabled'?: boolean;
+  'data-selected'?: boolean;
+  multiple?: boolean
+
+  onClick?: ItemProps['onClick'];
+  onMouseDown?: ItemProps['onMouseDown'];
+  onPointerMove?: ItemProps['onPointerMove'];
+  ref?: ItemProps['ref'];
+
+}
+
+export interface BaseCustomCommand<TValue = string | number, TData = undefined>
+  extends ForwardedProps {
+  option: Option<TValue, TData>;
+  isSelected: boolean;
+}

7a67c75a6cc7b38842150447c28865854765de84
Sergionx <sergio180203@gmail.com>
2025-08-18 15:00:59 -0400

    feat: implement fetchAndSetQueryData and invalidateRootQueryKey functions for query management

---
+++ client/src/lib/utils/tanstack-query.ts
@@ -0,0 +1,22 @@
+import { QueryClient } from '@tanstack/query-core';
+
+export async function fetchAndSetQueryData<T>(
+  queryClient: QueryClient,
+  queryKey: (string | number | object)[],
+  fetchFn: () => Promise<T>,
+): Promise<T> {
+  let data = queryClient.getQueryData<T>(queryKey);
+
+  if (!data) {
+    data = await fetchFn();
+    queryClient.setQueryData(queryKey, data);
+  }
+
+  return data;
+}
+
+export async function invalidateRootQueryKey(queryClient: QueryClient, queryKey: string | number) {
+  await queryClient.invalidateQueries({
+    predicate: (query) => Array.isArray(query.queryKey) && query.queryKey.length > 0 && query.queryKey[0] === queryKey,
+  });
+}

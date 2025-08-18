bd921189c72737d3036d18cabe136773d6e191a8
Sergionx <sergio180203@gmail.com>
2025-08-18 15:11:08 -0400

    fix: standardize quote style in authApi.ts for consistency

--- client/src/api/authApi.ts
+++ client/src/api/authApi.ts
@@ -1,5 +1,12 @@
-import axios from "@/axiosConfig";
+import axios from '@/axiosConfig';
+
+import type { Student } from '@/interfaces/Student';
+import type { RegisterStudentOutput } from '@/pages/(auth)/register/student/components/RegisterStudentForm/schema';
 
 export async function logOutGoogle() {
-  return await axios.get("/auth/google/logout");
+  return await axios.get('/auth/google/logout');
+}
+
+export async function completeStudent(userId: string, data: RegisterStudentOutput) {
+  return await axios.post<Student>(`/auth/${userId}/complete-student/`, data);
 }

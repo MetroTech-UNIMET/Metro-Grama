0294f47ac17f9286ea801fac26640d6d60971569
Sergionx <sergio180203@gmail.com>
2025-08-18 15:13:47 -0400

    feat: enhance useFormStepAsync with additional validation and error handling features

--- client/src/hooks/useFormStep.tsx
+++ client/src/hooks/useFormStep.tsx
@@ -1,10 +1,10 @@
 import { useState, useCallback } from 'react';
 
-import { zodErrorToFieldErrors } from '@utils/zod/zod-to-hook-form-errors';
-import { combinePaths, getZodPathFields } from '@utils/zod/zod-schema-paths';
+import { zodErrorToFieldErrors } from '@/lib/utils/zod/zod-to-hook-form-errors';
+import { combinePaths, getZodPathFields } from '@/lib/utils/zod/zod-schema-paths';
 
 import type { FieldErrors, FieldValues, Path, UseFormReturn } from 'react-hook-form';
-import type { FormSchema } from '@utils/zod/types';
+import type { FormSchema } from '@/lib/utils/zod/types';
 
 /**
  * Type representing the action to take when validating a step.
@@ -20,11 +20,14 @@ export interface Step {
   validateBeforeJumping?: ValidateAction;
 }
 
-interface Props<T extends FieldValues> {
+interface Props<T extends FieldValues, TTransformedValues = T> {
   steps: Step[];
-  form: UseFormReturn<T>;
+  form: UseFormReturn<T, unknown, TTransformedValues>;
   /**
    * Called when the form encounters an error when validating and if `callOnError` is true.
+   * @param errors The validation errors.
+   * @param currentStep The current step index.
+   * @param event The event that triggered the validation.
    */
   onError?: (
     errors: FieldErrors<T>,
@@ -33,23 +36,45 @@ interface Props<T extends FieldValues> {
   ) => unknown | Promise<unknown>;
   /**
    * Called when the schema validation is successful
+   * @param step The current step index.
+   * @param schema The schema used for validation.
+   * @param data The data after it was parsed by the schema.
    */
   onSuccess?: (step: number, schema: FormSchema, data: any) => void;
   /**
    * Function to transform the errors before comparing them with the schema path.
    * @description This is useful if you need to only get a subset of the form errors or transform them in some way before validation.
    * For example, you might want to remove some fields when wanting to validate a specific step.
+   * @param errors The form errors.
+   * @param currentStep The current step index.
+   * @returns The transformed form errors.
    */
   transformErrors?: (errors: FieldErrors<T>, currentStep: number) => FieldErrors<T>;
   /**
    * Optional function to filter the paths that will be validated.
    * @description This is useful if you want to validate only a subset of the paths in the schema.
+   * @param paths The paths of the form to filter.
+   * @param currentStep The current step index.
+   * @returns The filtered paths.
    */
   filterPaths?: (paths: Path<T>[], currentStep: number) => Path<T>[];
+  /**
+   * Called when the user navigates between pages. Useful for triggering side effects when the current
+   * page changes.
+   * @param prevPage The previous page number.
+   * @param nextPage The next page number.
+   */
   onPageChange?: (prevPage: number, nextPage: number) => void;
+
+  /**
+   * Called for additional validation before moving to the next step.
+   * @param currentStep The current step index.
+   * @returns A promise that resolves to a boolean indicating whether the validation passed.
+   */
+  extraValidation?: (currentStep: number) => Promise<boolean>;
 }
 
-export default function useFormStep<T extends FieldValues>({
+export default function useFormStepAsync<T extends FieldValues, TTransformedValues = T>({
   steps,
   form: { getValues, clearErrors, setError },
   onError,
@@ -57,7 +82,8 @@ export default function useFormStep<T extends FieldValues>({
   transformErrors,
   filterPaths,
   onPageChange,
-}: Props<T>) {
+  extraValidation,
+}: Props<T, TTransformedValues>) {
   const [currentStep, setCurrentStep] = useState(0);
   const stepsLength = steps.length;
 
@@ -66,118 +92,105 @@ export default function useFormStep<T extends FieldValues>({
       setCurrentStep(step);
       onPageChange?.(currentStep, step);
     },
-    [onPageChange],
+    [currentStep, onPageChange],
   );
 
   const validateFields = useCallback(
     async (currentStep: number, callOnError: boolean) => {
-      const currentSchema = steps[currentStep].schema;
-      if (!currentSchema) return true;
+      try {
+        const currentSchema = steps[currentStep].schema;
+        if (!currentSchema) return true;
 
-      const currentFormValues = getValues();
+        const currentFormValues = getValues();
 
-      const parsed = await currentSchema.safeParseAsync(currentFormValues);
+        const passExtraValidation = (await extraValidation?.(currentStep)) ?? true;
+        if (!passExtraValidation) return false;
 
-      const [pathFields, arrayPaths] = getZodPathFields(currentSchema);
-      let paths = generateRHFPaths(pathFields, arrayPaths, currentFormValues);
-      if (filterPaths) paths = filterPaths(paths, currentStep);
+        const parsed = await currentSchema.safeParseAsync(currentFormValues);
 
-      if (parsed.error) {
-        let rhfErrors = zodErrorToFieldErrors<T>(parsed.error, true);
-        const originalRhfErrors = rhfErrors;
-        if (transformErrors) rhfErrors = transformErrors(rhfErrors, currentStep);
+        const [pathFields, arrayPaths] = getZodPathFields(currentSchema);
+        let paths = generateRHFPaths(pathFields, arrayPaths, currentFormValues);
+        if (filterPaths) paths = filterPaths(paths, currentStep);
 
-        paths.forEach((path) => {
-          let { field, auxError } = getError();
+        if (parsed.error) {
+          let rhfErrors = zodErrorToFieldErrors<T>(parsed.error, true);
+          const originalRhfErrors = rhfErrors;
+          if (transformErrors) rhfErrors = transformErrors(rhfErrors, currentStep);
 
-          if (!auxError) {
-            clearErrors(field);
-            return;
-          }
-          const root = auxError?.root;
-          const message = auxError?.message;
+          paths.forEach((path) => {
+            const { field, auxError } = getError();
 
-          if (root) {
-            setError(field, root as any, {
-              shouldFocus: true,
-            });
-          } else if (message) {
-            setError(
-              field,
-              {
-                message: message as any,
-                type: auxError.type as any,
-                types: auxError.types as any,
-              },
-              {
+            if (!auxError) {
+              clearErrors(field);
+              return;
+            }
+            const root = auxError?.root;
+            const message = auxError?.message;
+
+            if (root) {
+              setError(field, root as any, {
                 shouldFocus: true,
-              },
-            );
-          } else {
-            // clearErrors(field);
-          }
-
-          function getError() {
-            const segments = path.split('.');
-
-            let field = '' as Path<T>;
-            let auxError = rhfErrors;
-
-            for (let i = 0; i < segments.length; i++) {
-              const segment = segments[i];
-              field = combinePaths(field, segment) as Path<T>;
-
-              auxError = auxError[segment] as FieldErrors<T>;
-              if (!auxError) return { field, auxError: undefined };
+              });
+            } else if (message) {
+              setError(
+                field,
+                {
+                  message: message as any,
+                  type: auxError.type as any,
+                  types: auxError.types as any,
+                },
+                {
+                  shouldFocus: true,
+                },
+              );
+            } else {
+              // clearErrors(field);
             }
 
-            return { field, auxError };
+            function getError() {
+              const segments = path.split('.');
+
+              let field = '' as Path<T>;
+              let auxError = rhfErrors;
+
+              for (let i = 0; i < segments.length; i++) {
+                const segment = segments[i];
+                field = combinePaths(field, segment) as Path<T>;
+
+                auxError = auxError[segment] as FieldErrors<T>;
+                if (!auxError) return { field, auxError: undefined };
+              }
+
+              return { field, auxError };
+            }
+          });
+
+          if (callOnError) {
+            await onError?.(originalRhfErrors, currentStep);
           }
-        });
-
-        if (callOnError) {
-          await onError?.(originalRhfErrors, currentStep);
+        } else {
+          paths.forEach((path) => {
+            clearErrors(path);
+          });
+          onSuccess?.(currentStep, currentSchema, parsed.data);
         }
-      } else {
-        paths.forEach((path) => {
-          clearErrors(path);
-        });
-        onSuccess?.(currentStep, currentSchema, parsed.data);
+
+        return parsed.success;
+      } catch (_) {
+        return false;
       }
-
-      return parsed.success;
     },
-    [steps, onError, transformErrors, onSuccess, filterPaths, getValues, setError, clearErrors],
-  );
-
-  const next = useCallback(
-    async (validate: ValidateAction = false) => {
-      if (!!validate) {
-        const validationPromise = validateFields(currentStep, validate === 'callOnError');
-        if (validate !== 'ignoreValidation' && !(await validationPromise)) {
-          return false;
-        }
-      }
-
-      if (currentStep < stepsLength - 1) changeStep(currentStep + 1);
-      return true;
-    },
-    [currentStep, stepsLength, validateFields, changeStep],
-  );
-
-  const previous = useCallback(
-    async (validate: ValidateAction = false) => {
-      if (!!validate) {
-        const validationPromise = validateFields(currentStep, validate === 'callOnError');
-        if (validate !== 'ignoreValidation' && !(await validationPromise)) {
-          return false;
-        }
-      }
-
-      if (currentStep > 0) changeStep(currentStep - 1);
-      return true;
-    },
-    [currentStep, validateFields, changeStep],
+    [
+      steps,
+      onError,
+      transformErrors,
+      onSuccess,
+      filterPaths,
+      extraValidation,
+      getValues,
+      setError,
+      clearErrors,
+    ],
   );
 
   const jumpToFirstErrorStep = useCallback(async () => {
@@ -191,10 +204,71 @@ export default function useFormStep<T extends FieldValues>({
     return true;
   }, [steps, validateFields, changeStep]);
 
+  const next = useCallback(
+    async (validate: ValidateAction = false) => {
+      if (validate) {
+        const validationPromise = validateFields(currentStep, validate === 'callOnError');
+        if (validate !== 'ignoreValidation' && !(await validationPromise)) {
+          return false;
+        }
+      }
+
+      const newStepIndex = currentStep + 1;
+      const nextStep = steps[newStepIndex];
+
+      if (nextStep.validateBeforeJumping) {
+        const isValid = await validateFields(
+          newStepIndex,
+          nextStep.validateBeforeJumping === 'callOnError',
+        );
+        if (!isValid) {
+          jumpToFirstErrorStep();
+          return false;
+        }
+      }
+
+      if (currentStep < stepsLength - 1) changeStep(newStepIndex);
+      return true;
+    },
+    [currentStep, stepsLength, validateFields, changeStep, steps, jumpToFirstErrorStep],
+  );
+
+  const previous = useCallback(
+    async (validate: ValidateAction = false) => {
+      if (validate) {
+        const validationPromise = validateFields(currentStep, validate === 'callOnError');
+        if (validate !== 'ignoreValidation' && !(await validationPromise)) {
+          return false;
+        }
+      }
+
+      const newStepIndex = currentStep - 1;
+      const nextStep = steps[newStepIndex];
+
+      if (nextStep.validateBeforeJumping) {
+        const isValid = await validateFields(
+          newStepIndex,
+          nextStep.validateBeforeJumping === 'callOnError',
+        );
+        if (!isValid) {
+          jumpToFirstErrorStep();
+          return false;
+        }
+      }
+
+      if (currentStep > 0) changeStep(newStepIndex);
+      return true;
+    },
+    [currentStep, validateFields, changeStep, steps, jumpToFirstErrorStep],
+  );
+
   const jumpTo = useCallback(
     async (step: number, validateCurrentStep: ValidateAction = false) => {
-      if (!!validateCurrentStep) {
-        const validationPromise = validateFields(currentStep, validateCurrentStep === 'callOnError');
+      if (validateCurrentStep) {
+        const validationPromise = validateFields(
+          currentStep,
+          validateCurrentStep === 'callOnError',
+        );
         if (validateCurrentStep !== 'ignoreValidation' && !(await validationPromise)) {
           return false;
         }
@@ -202,8 +276,11 @@ export default function useFormStep<T extends FieldValues>({
 
       const nextStep = steps[step];
 
-      if (steps[step].validateBeforeJumping) {
-        const isValid = await validateFields(step, nextStep.validateBeforeJumping === 'callOnError');
+      if (nextStep.validateBeforeJumping) {
+        const isValid = await validateFields(
+          step,
+          nextStep.validateBeforeJumping === 'callOnError',
+        );
         if (!isValid) {
           jumpToFirstErrorStep();
           return false;
@@ -248,7 +325,11 @@ function generateRHFPaths<T extends FieldValues>(
         });
       }
     } else {
-      resolveArrayPaths(`${basePath}.${currentSegment}`, restSegments, currentValue?.[currentSegment]);
+      resolveArrayPaths(
+        `${basePath}.${currentSegment}`,
+        restSegments,
+        currentValue?.[currentSegment],
+      );
     }
   }
 

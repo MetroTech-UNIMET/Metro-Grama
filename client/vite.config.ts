ba06a543c8912bf15eb53d3ddb9b643b5795f251
Sergionx <sergio180203@gmail.com>
2025-08-31 23:03:23 -0400

    chore: update dependencies and integrate TanStack Router
    - Added @tanstack/react-router and @tanstack/react-router-devtools to package.json
    - Removed react-router-dom and added recharts
    - Integrated TanStack Router plugin in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [ tanstackRouter(),react(), tailwindcss(),],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },

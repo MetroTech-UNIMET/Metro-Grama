4b58d4588b2a9241653165163f8db76b3ee6e723
Sergionx <sergio180203@gmail.com>
2025-09-05 11:09:37 -0400

    refactor: update SubjectSectionEntity structure for improved clarity and consistency

--- server/models/subject_section.go
+++ server/models/subject_section.go
@@ -4,7 +4,6 @@ import (
 	surrealModels "github.com/surrealdb/surrealdb.go/pkg/models"
 )
 
-// TODO - Cambiar los endpoints para que me retornen una lista de secciones que a su vez contienen
 type SubjectSectionEntity struct {
 	ID            surrealModels.RecordID `json:"id" validate:"required"  swaggertype:"object"`
 	ClassRoomCode *string                `json:"class_room_code"`

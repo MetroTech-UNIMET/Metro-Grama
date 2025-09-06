81d24d554126ffc3b2c9323cfb07a3fe8336d330
Sergionx <sergio180203@gmail.com>
2025-09-05 22:57:09 -0400

    feat: add GetCourseByTrimesterResponse interface to define course data structure

--- client/src/api/interactions/courseApi.ts
+++ client/src/api/interactions/courseApi.ts
@@ -1,8 +1,21 @@
 import axios from '@/axiosConfig';
 
+import type { GetCourseByTrimesterResponse } from './course.types';
+
 import type { Course } from '@/interfaces/Course';
 import type { CourseSchemaOutput } from '@/routes/horario/components/schema';
 
 export async function createSchedule(data: CourseSchemaOutput) {
   return await axios.post<Course>('/course/', data);
+}
+
+export interface QueryCourseParams {
+  is_principal?: boolean;
+}
+
+export async function getStudentCourseByTrimester(trimesterId: string, params: QueryCourseParams = {}) {
+  const query = new URLSearchParams();
+  if (params.is_principal !== undefined) query.append('is_principal', String(params.is_principal));
+  const { data } = await axios.get<GetCourseByTrimesterResponse>(`/course/${trimesterId}?${query.toString()}`);
+  return data;
 }

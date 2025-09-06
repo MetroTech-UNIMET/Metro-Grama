2d442b5989b1f10db4a13dd2a9172d9967fd92d9
Sergionx <sergio180203@gmail.com>
2025-09-05 23:26:07 -0400

    feat: include classroom_code in transformSection function for enhanced schedule data structure

--- client/src/features/weekly-schedule/weekly-schedule-sidebar/components/SubjectOfferDetail/SubjectOfferForm/functions.ts
+++ client/src/features/weekly-schedule/weekly-schedule-sidebar/components/SubjectOfferDetail/SubjectOfferForm/functions.ts
@@ -7,7 +7,8 @@ import type { SubjectScheduleInput, SubjectScheduleOutput } from './schema';
 import type { SubjectSectionWithSchedules } from '@/interfaces/SubjectSection';
 
 export function transformSection(sections: SubjectSectionWithSchedules[]): SubjectScheduleInput['sections'] {
-  return sections.map(({ schedules }) => ({
+  return sections.map(({ schedules,classroom_code }) => ({
+    classroom_code: classroom_code ?? undefined,
     schedules: schedules.map(
       ({
         day_of_week,

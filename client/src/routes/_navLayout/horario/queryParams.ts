7b364a0929e498ad35f258442ebbd6db153fc132
Sergionx <sergio180203@gmail.com>
2025-09-18 13:51:58 -0400

    feat: add trimester query parameter validation using zod

--- client/src/routes/_navLayout/horario/queryParams.ts
+++ client/src/routes/_navLayout/horario/queryParams.ts
@@ -1,10 +1,11 @@
 import z from 'zod/v4';
 
 import { careersQueryParam } from '@/routes/-common_queryParams/careers';
+import { trimesterQueryParam } from '@/routes/-common_queryParams/trimester';
 
 // TODO - Seguramente tenga un filtro para solamente ver electivas o por carrera
 export const queryParams = z.object({
-  trimester: z.union([z.literal('none'), z.string()]).catch('none'),
+  trimester: trimesterQueryParam,
   is_principal: z.boolean().default(true),
   careers: careersQueryParam,
 

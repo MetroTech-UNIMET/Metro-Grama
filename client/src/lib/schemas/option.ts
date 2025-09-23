924695508d7a071b71f4153ae990922b2ca9e4ed
Sergionx <sergio180203@gmail.com>
2025-09-23 10:49:27 -0400

    refactor: update option schemas to support union types and improve error handling

--- client/src/lib/schemas/option.ts
+++ client/src/lib/schemas/option.ts
@@ -1,18 +1,27 @@
-import { z } from "zod/v4";
+import { z } from 'zod/v4';
 
-export const optionSchema_NoTransform = z.object({
-  label: z.string(),
-  value: z.union([z.string(), z.number()]),
-});
-
-export const optionSchema = optionSchema_NoTransform.transform(
-  (data) => data.value
-);
+import type { $ZodErrorMap, $ZodIssueInvalidType, $ZodIssueUnrecognizedKeys } from 'zod/v4/core';
 
 export const optionStringSchema_NoTransform = z.object({
   label: z.string(),
   value: z.string(),
 });
-export const optionStringSchema = optionStringSchema_NoTransform.transform(
-  (data) => data.value
-);
+export const optionStringSchema = optionStringSchema_NoTransform.transform((data) => data.value);
+
+export function createOptionSchema(
+  error:
+    | string
+    | $ZodErrorMap<NonNullable<$ZodIssueInvalidType<unknown> | $ZodIssueUnrecognizedKeys>>
+    | undefined = undefined,
+  valueSchema: z.ZodType<any> = z.string(),
+) {
+  return z.object(
+    {
+      label: z.string(),
+      value: valueSchema,
+    },
+    {
+      error: error,
+    },
+  );
+}

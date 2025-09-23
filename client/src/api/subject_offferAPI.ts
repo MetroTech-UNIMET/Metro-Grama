f4769ecb3e4b4e545b735df7479789f3f02668fa
Sergionx <sergio180203@gmail.com>
2025-09-23 18:32:49 -0400

    feat: add create, delete, and upload functions for subject offers with type support

--- client/src/api/subject_offferAPI.ts
+++ client/src/api/subject_offferAPI.ts
@@ -1,8 +1,9 @@
 import axios from '@/axiosConfig';
 
-import type { SubjectOfferWithSections } from '@/interfaces/SubjectOffer';
+import type { SubjectOffer, SubjectOfferWithSections } from '@/interfaces/SubjectOffer';
 import type { SubjectEntity } from '@/interfaces/Subject';
 import type { Id } from '@/interfaces/surrealDb';
+import type { SuccessResponse } from './types';
 
 export interface Query_AnnualOffers {
   careers?: string[];
@@ -42,4 +43,30 @@ export async function getAnnualOfferByYear(year: string, career: string | undefi
 
   const res = await axios.get(`/subject_offer/annual/${encodeURIComponent(year)}?${params.toString()}`);
   return res.data as AnnualOfferByYearItem[];
+}
+
+export async function createOfferForSubject(subjectId: string, trimesterId: Id) {
+  const response = await axios.post<SuccessResponse<SubjectOffer>>(`/subject_offer/${subjectId}`, { trimesterId });
+  return response.data;
+}
+
+export async function deleteSubjectOffer(subjectId: string, trimesterId: Id) {
+  const response = await axios.delete<SuccessResponse<SubjectOffer>>(`/subject_offer/${subjectId}`, {
+    data: { trimesterId },
+  });
+  return response.data;
+}
+
+export async function uploadAnnualOfferPDF(file: File) {
+  const formData = new FormData();
+  formData.append('file', file);
+
+  const res = await axios.post<
+    SuccessResponse<{
+      period: string;
+      subject_offers: SubjectOfferWithSections[];
+    }>
+  >(`/subject_offer/upload`, formData);
+
+  return res.data;
 }

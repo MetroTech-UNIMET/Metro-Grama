72e9b974c255f5ed3f3d645fa5e1c2f5dd0092d7
Sergionx <sergio180203@gmail.com>
2025-09-27 12:02:36 -0400

    feat: implement loader function to fetch annual offer data based on career and year

--- client/src/routes/_navLayout/oferta/index.tsx
+++ client/src/routes/_navLayout/oferta/index.tsx
@@ -2,11 +2,14 @@ import { createFileRoute } from '@tanstack/react-router';
 
 import { ofertaSearchSchema } from './queryParams';
 import { OfertaAcademicTable } from '@/features/oferta/OfertaAcademicTable';
+import { fetchAnnualOfferByYearOptions } from '@/hooks/queries/subject_offer/use-fetch-annual-offer-by-year';
 
 export const Route = createFileRoute('/_navLayout/oferta/')({
   validateSearch: ofertaSearchSchema,
   loaderDeps: ({ search: { career, year } }) => ({ career, year }),
-  loader: async ({ deps: { career, year } }) => ({ career, year }),
+  loader: async ({ deps: { career, year }, context: { queryClient } }) => {
+    queryClient.ensureQueryData(fetchAnnualOfferByYearOptions({ year, career }));
+  },
   component: OfertaRoute,
 });
 

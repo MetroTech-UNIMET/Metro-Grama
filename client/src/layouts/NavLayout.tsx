cffd1e2ff37e193d88a2b338f5ae6ac316deb947
Sergionx <sergio180203@gmail.com>
2025-10-03 12:21:57 -0400

    fix: update NavLayout component for improved structure and readability

--- client/src/layouts/NavLayout.tsx
+++ client/src/layouts/NavLayout.tsx
@@ -1,49 +1,58 @@
 import { Outlet } from '@tanstack/react-router';
-import { Home, Calendar, LayoutList, User  } from 'lucide-react';
+import { Home, Calendar, LayoutList, User } from 'lucide-react';
+import { useAuth } from '@/contexts/AuthenticationContext';
 
 import MenuDockLink, { type DockLinkItem } from '@ui/shadcn-io/menu-dock/menu-dock-link';
+import { useMemo } from 'react';
 
 const LayoutListInverted = () => <LayoutList className="rotate-180" />;
 
-const items: DockLinkItem[] = [
-  {
-    label: 'Materias',
-    to: '/materias',
-    icon: Home,
-    search: (prev) => ({
-      careers: prev?.careers ?? [],
-    }),
-  },
-  {
-    label: 'Horario',
-    to: '/horario',
-    search: (prev) => ({
-      careers: prev?.careers ?? [],
-      trimester: 'none',
-      is_principal: true,
-      search: '',
-      orderBy: 'name',
-      filterByDays: [],
-    }),
-    icon: Calendar,
-  },
-  {
-    label: 'Oferta anual',
-    to: '/oferta',
-    search: (prev) => ({
-      careers: prev?.careers ?? [],
-      year: 'none',
-    }),
-    icon: LayoutListInverted,
-  },
-  {
-    label: 'Perfil',
-    to: '/profile',
-    icon: User,
-  },
-];
+function buildBaseItems(): DockLinkItem[] {
+  return [
+    {
+      label: 'Materias',
+      to: '/materias',
+      icon: Home,
+      search: (prev) => ({
+        careers: prev?.careers ?? [],
+      }),
+    },
+    {
+      label: 'Horario',
+      to: '/horario',
+      search: (prev) => ({
+        careers: prev?.careers ?? [],
+        trimester: 'none',
+        is_principal: true,
+        search: '',
+        orderBy: 'name',
+        filterByDays: [],
+      }),
+      icon: Calendar,
+    },
+    {
+      label: 'Oferta anual',
+      to: '/oferta',
+      search: (prev) => ({
+        careers: prev?.careers ?? [],
+        year: 'none',
+      }),
+      icon: LayoutListInverted,
+    },
+  ];
+}
 
 export function NavLayout({ children }: { children?: React.ReactNode }) {
+  const { status } = useAuth();
+
+  const items = useMemo(() => {
+    const baseItems = buildBaseItems();
+    if (status === 'authenticated') {
+      baseItems.push({ label: 'Perfil', to: '/profile', icon: User });
+    }
+    return baseItems;
+  }, [status]);
+
   return (
     <>
       {children ? children : <Outlet />}

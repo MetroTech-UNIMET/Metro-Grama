1ad14264b51371003bc6d6aa81b3a7cce1f5ff27
Sergionx <sergio180203@gmail.com>
2025-10-07 22:46:37 -0400

    refactor: streamline GetSectionsWithSchedules by extracting subquery logic for improved readability

--- server/modules/interactions/course/services/query_course.service.go
+++ server/modules/interactions/course/services/query_course.service.go
@@ -13,25 +13,9 @@ import (
 
 // GetSectionsWithSchedules retrieves principal or secondary sections (depending on isPrincipal) and their schedules for a student and trimester.
 func GetSectionsWithSchedules(studentId surrealModels.RecordID, trimesterId string, isPrincipal bool) ([]DTO.QueryCourse, error) {
-	sectionField := "principal_sections"
-	if !isPrincipal {
-		sectionField = "secondary_sections"
-	}
-
-	schedule_Subquery := surrealql.Select("subject_schedule").Field("*").
-		Where("subject_section = $parent.id").
-		OrderBy("day_of_week").
-		OrderBy("starting_hour").
-		OrderBy("starting_minute")
-
-	section_Subquery := surrealql.Select(fmt.Sprintf("$parent.%s", sectionField)).
-		Field("*").
-		Alias("subject", "subject_offer.in").
-		Alias("subject_schedule", schedule_Subquery).
-		Fetch("subject")
 
 	qb := surrealql.SelectOnly("course").
-		Value(section_Subquery).
+		Value(GetSectionSubquery(isPrincipal)).
 		WhereEq("out", surrealModels.NewRecordID("trimester", trimesterId)).
 		WhereEq("in", studentId)
 
@@ -49,4 +33,24 @@ func GetSectionsWithSchedules(studentId surrealModels.RecordID, trimesterId stri
 		return []DTO.QueryCourse{}, nil
 	}
 	return sections, nil
+}
+
+func getScheduleSubquery() *surrealql.SelectQuery {
+	return surrealql.Select("subject_schedule").Field("*").
+		Where("subject_section = $parent.id").
+		OrderBy("day_of_week").
+		OrderBy("starting_hour").
+		OrderBy("starting_minute")
+}
+func GetSectionSubquery(isPrincipal bool) *surrealql.SelectQuery {
+	sectionField := "principal_sections"
+	if !isPrincipal {
+		sectionField = "secondary_sections"
+	}
+
+	return surrealql.Select(fmt.Sprintf("$parent.%s", sectionField)).
+		Field("*").
+		Alias("subject", "subject_offer.in").
+		Alias("subject_schedule", getScheduleSubquery()).
+		Fetch("subject")
 }

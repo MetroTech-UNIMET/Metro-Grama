93a8a48e02d8d4554edb66d3d0c44a370d1c2e84
Sergionx <sergio180203@gmail.com>
2025-10-16 21:16:30 -0400

    refactor: improve error handling in CreateCourse function by consolidating error checks

--- server/modules/interactions/course/services/create_course.service.go
+++ server/modules/interactions/course/services/create_course.service.go
@@ -10,21 +10,9 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/surrealdb/surrealdb.go"
+	"github.com/surrealdb/surrealdb.go/contrib/surrealql"
 	surrealModels "github.com/surrealdb/surrealdb.go/pkg/models"
 )
-
-var createQuery = ` 
-BEGIN TRANSACTION; 
-	LET $deleted = DELETE ONLY course 
-        WHERE in = $studentId AND out = $trimesterId
-        RETURN BEFORE;
-
-	RETURN RELATE ONLY $studentId->course->$trimesterId SET 
-        principal_sections = $data.principal_sections ?? $deleted.principal_sections,
-        secondary_sections = $data.secondary_sections ?? $deleted.secondary_sections;
-
-COMMIT TRANSACTION;
-`
 
 func CreateCourse(studentId surrealModels.RecordID, input DTO.CreateCourse) (models.CourseEntity, error) {
 	data := map[string]any{}
@@ -35,18 +23,43 @@ func CreateCourse(studentId surrealModels.RecordID, input DTO.CreateCourse) (mod
 	}
 
 	params := map[string]any{
-		"studentId":   studentId,
-		"trimesterId": input.TrimesterId,
-		"data":        data,
+		"data": data,
 	}
 
-	res, err := surrealdb.Query[models.CourseEntity](context.Background(), db.SurrealDB, createQuery, params)
-	errorMessage := tools.ExtractSurrealErrorMessage(err.Error())
-	switch errorMessage {
-	case "All the sections from principal_sections must belong to the same trimester of the schedule":
-		return models.CourseEntity{}, echo.NewHTTPError(http.StatusBadRequest, "Todas las secciones del horario principal deben pertenecer al mismo trimestre ")
-	case "All the sections from secondary_sections must belong to the same trimester of the schedule":
-		return models.CourseEntity{}, echo.NewHTTPError(http.StatusBadRequest, "Todas las secciones del horario secundario deben pertenecer al mismo trimestre ")
+	delete_Qb := surrealql.Delete("course").
+		Where("in = $studentId").
+		Where("out = $trimesterId").
+		ReturnBefore()
+
+	relate_Qb := surrealql.RelateOnly("$studentId", "course", "$trimesterId")
+
+	if input.IsPrincipal {
+		relate_Qb = relate_Qb.Set("principal_sections = ? ?? $deleted.principal_sections", input.Sections).
+			Set("secondary_sections = $deleted.secondary_sections ?? []")
+	} else {
+		relate_Qb = relate_Qb.Set("secondary_sections = ? ?? $deleted.secondary_sections", input.Sections).
+			Set("principal_sections = $deleted.principal_sections ?? []")
+	}
+
+	qb := surrealql.Begin().
+		Let("deleted", surrealql.Expr("?[0]", delete_Qb)).
+		Return("?", relate_Qb)
+
+	sql, params := qb.Build()
+
+	params["studentId"] = studentId
+	params["trimesterId"] = input.TrimesterId
+	params["data"] = data
+
+	res, err := surrealdb.Query[models.CourseEntity](context.Background(), db.SurrealDB, sql, params)
+	if err != nil {
+		errorMessage := tools.ExtractSurrealErrorMessage(err.Error())
+		switch errorMessage {
+		case "All the sections from principal_sections must belong to the same trimester of the schedule":
+			return models.CourseEntity{}, echo.NewHTTPError(http.StatusBadRequest, "Todas las secciones del horario principal deben pertenecer al mismo trimestre ")
+		case "All the sections from secondary_sections must belong to the same trimester of the schedule":
+			return models.CourseEntity{}, echo.NewHTTPError(http.StatusBadRequest, "Todas las secciones del horario secundario deben pertenecer al mismo trimestre ")
+		}
 	}
 
 	if err != nil {
@@ -55,5 +68,3 @@ func CreateCourse(studentId surrealModels.RecordID, input DTO.CreateCourse) (mod
 	course := (*res)[0].Result
 	return course, nil
 }
-
-// VivaFlembe1+

6aec040f0e3a6410cf194a1c6d5022ff0bfa87b8
Sergionx <sergio180203@gmail.com>
2025-10-30 16:31:41 -0400

    fix: add ref prop to TrimesterItem component for improved accessibility

--- client/src/components/ui/derived/custom-command-items/trimester-item-option.tsx
+++ client/src/components/ui/derived/custom-command-items/trimester-item-option.tsx
@@ -6,7 +6,7 @@ import type { Trimester } from '@/interfaces/Trimester';
 import type { BaseCustomCommand } from './types';
 
 type Data = Trimester;
-export function TrimesterItem<T extends Data>({ option, isSelected, ...props }: BaseCustomCommand<string, T>) {
+export function TrimesterItem<T extends Data>({ option, isSelected, ref, ...props }: BaseCustomCommand<string, T>) {
   if (!option.data) throw new Error('Option data is required for TrimesterItem component');
 
   const { is_current, is_next } = option.data;
@@ -14,14 +14,16 @@ export function TrimesterItem<T extends Data>({ option, isSelected, ...props }: 
   return (
     <div
       className={cn(
-        'flex gap-2 cursor-pointer flex-row flex-wrap items-center rounded px-2 py-1.5 text-sm',
+        'flex cursor-pointer flex-row flex-wrap items-center gap-2 rounded px-2 py-1.5 text-sm',
+        'data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50',
+        'border-secondary-800 not-[aria-selected]:pl-8 aria-selected:border',
         {
           'bg-secondary-100/50 hover:bg-secondary-100/70': is_next,
           'bg-primary-100/50': is_current,
-        },    
-        isSelected ? 'bg-secondary-100 dark:bg-secondary-800' : 'pl-8 hover:bg-gray-50 dark:hover:bg-gray-800',
-        props['aria-disabled'] && 'pointer-events-none opacity-50',
+        },
+        isSelected && 'bg-secondary-100 dark:bg-secondary-800',
       )}
+      ref={ref}
       {...props}
     >
       {isSelected && <Check className="w-4" />}

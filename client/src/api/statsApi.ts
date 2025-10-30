18381758565acbcb89dd6062adc2a2f7a296bdff
Sergionx <sergio180203@gmail.com>
2025-10-30 16:34:37 -0400

    fix: enhance fetchSubjectStatsOptions and useFetchSubjectStats for improved type safety and query handling

--- client/src/api/statsApi.ts
+++ client/src/api/statsApi.ts
@@ -2,8 +2,19 @@ import axios from '@/axiosConfig';
 
 import type { SubjectStats } from '@/interfaces/Subject';
 
-export async function getSubjectStats(subjectId: string) {
-  const response = await axios.get<SubjectStats[]>(`/stats/subjects/${subjectId}`);
+export interface Query_SubjectStats {
+  studentFilter?: 'all' | 'friends' | 'friendFriends';
+  careers?: string[];
+  startingTrimester?: string;
+  endingTrimester?: string;
+}
+
+export async function getSubjectStats(subjectId: string, query?: Query_SubjectStats) {
+  const { careers, ...restQuery } = query ?? {};
+
+  const newQuery = { ...restQuery, careers: careers?.join(',') };
+
+  const response = await axios.get<SubjectStats[]>(`/stats/subjects/${subjectId}`, { params: newQuery });
 
   return response.data;
 }
--- client/src/hooks/queries/stats/use-fetch-subject-stats.tsx
+++ client/src/hooks/queries/stats/use-fetch-subject-stats.tsx
@@ -1,19 +1,29 @@
 import { queryOptions, useQuery } from '@tanstack/react-query';
 
-import { getSubjectStats } from '@/api/statsApi';
+import { getSubjectStats, Query_SubjectStats } from '@/api/statsApi';
 
 import type { SubjectStats } from '@/interfaces/Subject';
 import type { OptionalQueryOptions } from '../types';
 
-export function fetchSubjectStatsOptions(subjectCode: string, queryOpt?: OptionalQueryOptions<SubjectStats[]>) {
+interface Props<T = SubjectStats[]> {
+  queryOptions?: OptionalQueryOptions<T>;
+  optionalQuery?: Query_SubjectStats;
+}
+
+export function fetchSubjectStatsOptions(subjectCode: string, { queryOptions: queryOpt, optionalQuery }: Props = {}) {
+  const { careers, ...restQuery } = optionalQuery ?? {};
+  const careerOrdered = careers?.sort()
+
+  const newQuery = { ...restQuery, careers: careerOrdered};
+
   return queryOptions({
-    queryKey: ['subjects', subjectCode, 'stat'],
-    queryFn: () => getSubjectStats(subjectCode),
+    queryKey: ['subjects', subjectCode, 'stats', newQuery],
+    queryFn: () => getSubjectStats(subjectCode, newQuery),
     ...queryOpt,
   });
 }
 
-export function useFetchSubjectStats(subjectCode: string, queryOptions?: OptionalQueryOptions<SubjectStats[]>) {
-  const query = useQuery(fetchSubjectStatsOptions(subjectCode, queryOptions));
+export function useFetchSubjectStats(subjectCode: string, props: Props = {}) {
+  const query = useQuery(fetchSubjectStatsOptions(subjectCode, props));
   return query;
 }

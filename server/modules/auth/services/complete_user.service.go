bbff61f0189f4383682b80a59a5e8e25db861f1c
Sergionx <sergio180203@gmail.com>
2025-11-20 19:32:44 -0400

    refactor: clean up CompleteStudent function for improved readability and maintainability

--- server/modules/auth/services/complete_user.service.go
+++ server/modules/auth/services/complete_user.service.go
@@ -7,47 +7,41 @@ import (
 	"metrograma/modules/auth/DTO"
 
 	"github.com/surrealdb/surrealdb.go"
+	"github.com/surrealdb/surrealdb.go/contrib/surrealql"
 	surrealModels "github.com/surrealdb/surrealdb.go/pkg/models"
 )
 
-const completeUserQuery = `
-BEGIN TRANSACTION;
+func CompleteStudent(idUser string, data DTO.CompleteStudentDTO) (models.StudentWithUser, error) {
+	qb := surrealql.Begin().
+		Let("current", surrealql.SelectOnly("$userId").Field("verified")).
+		If("$current.verified == true").
+		Then(func(tb *surrealql.ThenBuilder) {
+			tb.Throw("User is already verified")
+		}).
+		End().
+		Do(surrealql.UpdateOnly("$userId").
+			Set("phone", "$update.phone").
+			Set("verified", true)).
+		Let("student", surrealql.CreateOnly("student").
+			Set("user", "$userId").
+			Set("id_card", "<string> $update.id_card")).
+		Let("studentId", "$student.id").
+		Do(surrealql.For("careerTri", "?", "$update.careersWithTrimesters").
+			LetTyped("careerId", "<record<career>>", "$careerTri.career").
+			LetTyped("trimesterId", "<record<trimester>>", "$type::thing('trimester',  $careerTri.trimester)").
+			Do(surrealql.Relate("$studentId", "study", "$careerId").
+				Set("startingTrimester", "$trimesterId")),
+		).
+		Do(surrealql.CreateOnly("student_preferences").
+			Set("student", "$studentId")).
+		Return("$student FETCH user")
 
-LET $current = SELECT verified FROM ONLY $userId;
-IF $current.verified == true {
-    THROW "User is already verified";
-};
-	
-UPDATE ONLY $userId SET
-		phone = $update.phone,
-		verified = true;
+	sql, params := qb.Build()
 
-LET $student =
-    CREATE ONLY student SET
-    user = $userId,
-    id_card = <string> $update.id_card;
-LET $studentId = $student.id;
+	params["userId"] = surrealModels.NewRecordID("user", idUser)
+	params["update"] = data
 
-FOR $careerTri IN $update.careersWithTrimesters {
-    LET $careerId = <record<career>> $careerTri.career;
-    LET $trimesterId = <record<trimester>>
-        type::thing('trimester',  $careerTri.trimester);
-    RELATE $studentId -> study ->$careerId SET
-        startingTrimester = $trimesterId;
-};
-
-RETURN $student FETCH user;
-
-COMMIT TRANSACTION;
-`
-
-func CompleteUser(idUser string, data DTO.CompleteStudentDTO) (any, error) {
-	queryParams := map[string]any{
-		"userId": surrealModels.NewRecordID("user", idUser),
-		"update": data,
-	}
-
-	result, err := surrealdb.Query[models.StudentWithUser](context.Background(), db.SurrealDB, completeUserQuery, queryParams)
+	result, err := surrealdb.Query[models.StudentWithUser](context.Background(), db.SurrealDB, sql, params)
 
 	user := (*result)[0].Result
 

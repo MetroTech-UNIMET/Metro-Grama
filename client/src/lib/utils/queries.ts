d55f8798a3b86566255c5afefc1e4e166c27bce2
Sergionx <sergio180203@gmail.com>
2025-11-28 11:34:40 -0400

    refactor: improve type imports and formatting in queries.ts for consistency

--- client/src/lib/utils/queries.ts
+++ client/src/lib/utils/queries.ts
@@ -1,13 +1,10 @@
-import { AxiosError } from "axios";
+import type { AxiosError } from 'axios';
+import type { Id } from '@/interfaces/surrealDb';
 
 //FIXME - Mejorar el tipado del error
-export function notRetryOnUnauthorized<T>(
-  failureCount: number,
-  error: AxiosError<T>,
-  maxRetries = 3
-) {
+export function notRetryOnUnauthorized<T>(failureCount: number, error: AxiosError<T>, maxRetries = 3) {
   // Don't retry if the error message is "Unauthorized"
-  if ((error as any)?.response?.data?.message === "Unauthorized") {
+  if ((error as any)?.response?.data?.message === 'Unauthorized') {
     return false;
   }
 
@@ -16,4 +13,8 @@ export function notRetryOnUnauthorized<T>(
 
 export function idToSurrealId(id: string, table: string) {
   return `${table}:${id}`;
+}
+
+export function surrealIdToId(surrealId: Id) {
+  return idToSurrealId(surrealId.Table, surrealId.ID);
 }

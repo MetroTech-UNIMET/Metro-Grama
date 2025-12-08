83e0025147cea4eff86302869965a17fb723fc75
Sergionx <sergio180203@gmail.com>
2025-12-08 19:23:06 -0400

    refactor: standardize import quotes and update createCareer function type definition

--- client/src/api/careersApi.ts
+++ client/src/api/careersApi.ts
@@ -1,21 +1,23 @@
-import axios from "@/axiosConfig";
+import axios from '@/axiosConfig';
 
-import type { Career, CareerWithSubjects } from "@/interfaces/Career";
+import type { CreateCareerOutput } from '@/features/admin/careers/schema';
+import type { Career, CareerWithSubjects } from '@/interfaces/Career';
 
 export async function getCareers(): Promise<Career[]> {
-  const response = await axios.get("/careers/");
+  const response = await axios.get('/careers/');
 
   return response.data;
 }
 
-export async function createCareer(data: any) {
-  return await axios.post("/careers/", data);
+// El casting del tipo subjects es complicaíto
+export async function createCareer(data: Omit<CreateCareerOutput, 'subjects'>) {
+  return await axios.post('/careers/', data);
 }
 
 export async function updateCareer(oldCareer: CareerWithSubjects, newCareer: any) {
   return await axios.patch(`/careers/withSubjects/${oldCareer.id}`, {
     oldCareer,
-    newCareer
+    newCareer,
   });
 }
 

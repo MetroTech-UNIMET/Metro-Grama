1743d052460d4f7c27c92d8599d92243ff8f5f34
Sergionx <sergio180203@gmail.com>
2026-01-22 21:45:45 -0400

    feat: refactor Slider component to render multiple thumbs based on value props

--- client/src/components/ui/slider.tsx
+++ client/src/components/ui/slider.tsx
@@ -15,7 +15,12 @@ function Slider({ className, ...props }: React.ComponentProps<typeof SliderPrimi
       <SliderPrimitive.Track className="bg-primary/20 relative h-1.5 w-full grow overflow-hidden rounded-full dark:bg-neutral-50/20">
         <SliderPrimitive.Range className="bg-primary data-[disabled=true]:bg-primary/60 absolute h-full dark:bg-neutral-50" />
       </SliderPrimitive.Track>
-      <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-neutral-900/50 bg-white shadow-sm transition-colors focus-visible:ring-1 focus-visible:ring-neutral-950 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-800 dark:bg-neutral-950 dark:focus-visible:ring-neutral-300" />
+      {(props.value || props.defaultValue || [0]).map((_, i) => (
+        <SliderPrimitive.Thumb
+          key={i}
+          className="block h-4 w-4 rounded-full border border-neutral-900/50 bg-white shadow-sm transition-colors focus-visible:ring-1 focus-visible:ring-neutral-950 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-800 dark:bg-neutral-950 dark:focus-visible:ring-neutral-300"
+        />
+      ))}
     </SliderPrimitive.Root>
   );
 }

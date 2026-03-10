970bfd187bb7c89aa78d983b2e68795d5a59533e
Sergionx <sergio180203@gmail.com>
2026-03-10 17:14:35 -0400

    fix: update label format for prelations in CareerForm to include subject code

--- client/src/features/admin/careers/CareerForm.tsx
+++ client/src/features/admin/careers/CareerForm.tsx
@@ -74,7 +74,7 @@ export default function CareerForm({ mode, data }: Props) {
             BPCredits: subject.BPCredits,
             prelations: subject.prelations.map(({ ID }) => ({
               value: ID,
-              label: subjectsById[ID] ?? 'Materia no encontrada',
+              label: subjectsById[ID] ? `(${subjectCode}) - ${subjectsById[ID]}` : 'Materia no encontrada',
             })),
           };
 

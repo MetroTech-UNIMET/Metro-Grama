1eff9eae20e649387bdfb64a1fc261930e562f94
Sergionx <sergio180203@gmail.com>
2026-03-18 10:42:12 -0400

    feat: add Date field to SubjectStat for improved data tracking

--- server/modules/stats/DTO/subject_stat.dto.go
+++ server/modules/stats/DTO/subject_stat.dto.go
@@ -3,9 +3,10 @@ package DTO
 import surrealModels "github.com/surrealdb/surrealdb.go/pkg/models"
 
 type SubjectStat struct {
-	Count      uint                   `json:"count"`
-	Difficulty float32                `json:"difficulty"`
-	Grade      float32                `json:"grade"`
-	Workload   float32                `json:"workload"`
-	Trimester  surrealModels.RecordID `json:"trimester" swaggertype:"object"`
+	Count      uint                         `json:"count"`
+	Date       surrealModels.CustomDateTime `json:"date"`
+	Difficulty float32                      `json:"difficulty"`
+	Grade      float32                      `json:"grade"`
+	Workload   float32                      `json:"workload"`
+	Trimester  surrealModels.RecordID       `json:"trimester" swaggertype:"object"`
 }

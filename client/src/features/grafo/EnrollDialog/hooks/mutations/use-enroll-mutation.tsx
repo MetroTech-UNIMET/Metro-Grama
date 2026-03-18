a0a42c480f19a4a57d2d8d022d7a138a17e99660
Sergionx <sergio180203@gmail.com>
2026-03-18 11:58:28 -0400

    fix: optimize query invalidation logic in useMutationEnrollSubject for improved performance

--- client/src/features/grafo/EnrollDialog/hooks/mutations/use-enroll-mutation.tsx
+++ client/src/features/grafo/EnrollDialog/hooks/mutations/use-enroll-mutation.tsx
@@ -23,16 +23,19 @@ export function useMutationEnrollSubject({ subjectCode, afterSubmit }: Props) {
     },
     onSuccess: async (result, { data }) => {
       if (!subjectCode) return;
-      await queryClient.invalidateQueries({
-        queryKey: queryKeys.subjects.details(subjectCode)._ctx.stats(undefined).queryKey,
-        refetchType: 'all',
-      });
-      await queryClient.invalidateQueries({
-        queryKey: queryKeys.student.details('my-id').queryKey,
-        refetchType: 'all',
-      });
-      await queryClient.invalidateQueries({ queryKey:  queryKeys.subjectOffers._def, refetchType: 'all' }); //REVIEW Ya ni me acuerdo porq ue invalidaba esto, en verdad es necesario? Capaz para cuando es especifica por trimestre por el tema de los stats
-      await queryClient.invalidateQueries({ queryKey: queryKeys.student.enrolledSubjects._def });
+
+      await Promise.all([
+        queryClient.invalidateQueries({
+          queryKey: queryKeys.subjects.details(subjectCode)._ctx.stats(undefined).queryKey,
+          refetchType: 'all',
+        }),
+        queryClient.invalidateQueries({
+          queryKey: queryKeys.student.details('my-id').queryKey,
+          refetchType: 'all',
+        }),
+        queryClient.invalidateQueries({ queryKey: queryKeys.subjectOffers._def, refetchType: 'all' }), //REVIEW Ya ni me acuerdo porq ue invalidaba esto, en verdad es necesario? Capaz para cuando es especifica por trimestre por el tema de los stats
+        queryClient.invalidateQueries({ queryKey: queryKeys.student.enrolledSubjects._def }),
+      ]);
 
       toast.success('Materia marcada exitosamente', {
         description: result.message || 'La materia se marcó como cursada',
